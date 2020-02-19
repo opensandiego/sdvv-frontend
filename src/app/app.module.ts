23ff2c8c96e5651c286ea5134631a33d265d5340
gSoD17 <galentro451@gmail.com>
2020-02-18 19:20:26 -0800

    added angular material and primeng ui library to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashPageComponent } from './components/splash-page/splash-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SplashPageComponent } from './components/splash-page/splash-page.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
