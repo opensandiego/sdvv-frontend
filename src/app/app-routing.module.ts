8a61205ff2464fd8486b12325014941f95886812
gSoD17 <galentro451@gmail.com>
2020-02-18 20:31:00 -0800

    removed splash page component and moved its contents into the main app files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SplashPageComponent } from './components/splash-page/splash-page.component';
+// import { SplashPageComponent } from './components/splash-page/splash-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'splash', pathMatch: 'full' },
-  { path: 'splash', component: SplashPageComponent },
+  // { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  // { path: 'splash', component: SplashPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SplashPageComponent } from './components/splash-page/splash-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashPageComponent
   ],
   imports: [
     BrowserModule,
