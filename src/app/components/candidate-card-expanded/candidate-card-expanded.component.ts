a828a19b8847fa9cd6e3bf7a8ac295ef10764c05
gSoD17 <galentro451@gmail.com>
2020-04-14 20:33:50 -0700

    generated new component for expanded card

---
+++ src/app/components/candidate-card-expanded/candidate-card-expanded.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-card-expanded',
+  templateUrl: './candidate-card-expanded.component.html',
+  styleUrls: ['./candidate-card-expanded.component.scss']
+})
+export class CandidateCardExpandedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
