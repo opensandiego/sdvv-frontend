c93e55f4dd59fbbea0b18dec01416c25c2873c6c
Phil Melendez <pmelendez@tecocllc.com>
2020-07-01 22:25:18 -0700

    Add Firebase configuration and fix compile erros in TS

--- src/app/components/candidate-card/candidate-card.component.ts
+++ src/app/components/candidate-card/candidate-card.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit, Input, Injectable } from '@angular/core';
+import { Component, OnInit, Input, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Candidate } from '../../candidate';
+import { Candidate } from "../../candidate";
 @Component({
-  selector: 'app-candidate-card',
-  templateUrl: './candidate-card.component.html',
-  styleUrls: ['./candidate-card.component.scss']
+  selector: "app-candidate-card",
+  templateUrl: "./candidate-card.component.html",
+  styleUrls: ["./candidate-card.component.scss"],
 })
-
 export class CandidateCardComponent implements OnInit {
-  private canRender = false;
+  public canRender = false;
   data: any[];
-  
+
   @Input() imageUrl: string = "../../assets/candidate-card/candidate.jpg";
   @Input() jsonFile: string;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get("assets/candidates/2020/mayor/" + this.jsonFile)
+    this.http
+      .get("assets/candidates/2020/mayor/" + this.jsonFile)
       .toPromise()
-      .then(res => {
+      .then((res) => {
         this.data = [];
         this.data.push(res);
         console.log(this.data);
         this.canRender = true;
       });
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDD1Cxs3-Nx2R99K1UoxhfOs8Q68fJezyw",
+    authDomain: "sdvv-firebase-git.firebaseapp.com",
+    databaseURL: "https://sdvv-firebase-git.firebaseio.com",
+    projectId: "sdvv-firebase-git",
+    storageBucket: "sdvv-firebase-git.appspot.com",
+    messagingSenderId: "375018032962",
+    appId: "1:375018032962:web:620326f5ed692286dc02db",
+  },
 };
 
 /*
