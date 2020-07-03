82a73755127a94c004027f71fd3e7963a1dba136
Phil Melendez <33038363+pmelendez2@users.noreply.github.com>
2020-07-03 15:55:45 -0700

    Github Action yaml file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,13 @@ export const environment = {
   environmentName: 'Development',
   environmentUrl: 'http://localhost',
   firebase: {
-    apiKey: 'AIzaSyDD1Cxs3-Nx2R99K1UoxhfOs8Q68fJezyw',
-    authDomain: 'sdvv-firebase-git.firebaseapp.com',
-    databaseURL: 'https://sdvv-firebase-git.firebaseio.com',
-    projectId: 'sdvv-firebase-git',
-    storageBucket: 'sdvv-firebase-git.appspot.com',
-    messagingSenderId: '375018032962',
-    appId: '1:375018032962:web:620326f5ed692286dc02db'
+    apiKey: "AIzaSyDD1Cxs3-Nx2R99K1UoxhfOs8Q68fJezyw",
+    authDomain: "sdvv-firebase-git.firebaseapp.com",
+    databaseURL: "https://sdvv-firebase-git.firebaseio.com",
+    projectId: "sdvv-firebase-git",
+    storageBucket: "sdvv-firebase-git.appspot.com",
+    messagingSenderId: "375018032962",
+    appId: "1:375018032962:web:a2c898c27a2529bcdc02db"
   }
 }
 
