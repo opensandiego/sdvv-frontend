c17d626ec67c00f42d23aa82b5cb1fce2abe62ef
Bolun Thompson <abolunthompson@gmail.com>
2020-07-16 09:49:13 -0700

    Added documentation

--- src/scripts/calculation_download_gdrive_info.py
+++ src/scripts/calculation_download_gdrive_info.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""
+Reads and updates the candidate JSON files from the candidate spreadsheet.
+
+Reads the information from the speadsheet (https://docs.google.com/spreadsheets/d/1mENueYg0PhXE_MA9AypWWBJvBLdY03b8H_N_aIW-Ohw/)
+and generates or updates the candidate JSON files. This updates the:
+candidate name,
+first name,
+last name,
+description,
+website,
+and commitee name.
+"""
 import json
 import math
 import os
@@ -9,6 +21,7 @@ import pandas as pd
 
 
 def replace_nan(value, replace):
+    """Replaces param value with param replace if value is NaN."""
     try:
         if math.isnan(value):
             return replace
@@ -18,6 +31,14 @@ def replace_nan(value, replace):
 
 
 def read_candidate_csv(file):
+    """
+    Reads a Pandas Dataframe from a CSV file with candidate information.
+
+    If the row is completely empty, drops it.
+
+    :param file: A CSV file path (including URLS) or object that has
+    the columns being read.
+    """
     return (
         pd.read_csv(
             file,
@@ -35,6 +56,14 @@ def read_candidate_csv(file):
 
 
 def normalize(string, nan_replacement=None):
+    """
+    Normalizes strings by replacing spaces with underscores and lowercases it.
+
+    If nan_replacement is not None, NaN values are replaced with it.
+
+    :param string: A str to be normalized.
+    :param nan_replacement: The optional value NaN values are replaced with.
+    """
     try:
         if nan_replacement is not None and math.isnan(string):
             return nan_replacement
@@ -44,16 +73,25 @@ def normalize(string, nan_replacement=None):
 
 
 class JsonFilesNT(typing.NamedTuple):
+    """Named tuple that contains the contents of a JSON file and the file's path."""
+
     contents: dict
     path: str
 
 
-def generate_json_files(base_directory, candidate_csv):
+def generate_json_files(base_directory, candidate_df):
+    """
+    Creates empty candidate JSON files in a folder.
+
+    :param base_directory: The str path of the directory all the files
+    and folders will be generated in.
+    :param candidate_df: The Pandas Dataframe the data will be taken from.
+    """
     files = {}
-    for candidate in candidate_csv.index:
+    for candidate in candidate_df.index:
         dir_path = "{}{}/{}/".format(
             base_directory,
-            normalize(candidate_csv.loc[candidate]["Office"], "other"),
+            normalize(candidate_df.loc[candidate]["Office"], "other"),
             normalize(candidate),
         )
         os.makedirs(dir_path, exist_ok=True)
@@ -72,8 +110,14 @@ def generate_json_files(base_directory, candidate_csv):
     return files
 
 
-def update_json_files(folder_path, sheet_url):
-    with request.urlopen(sheet_url) as f:
+def update_json_files(folder_path, csv_url):
+    """
+    Generates and updates candidate JSON files in a folder.
+
+    :param folder_path: The str path of the folder the JSON files will be in.
+    :param csv_url: The str url of the CSV file that the data will be taken from.
+    """
+    with request.urlopen(csv_url) as f:
         csv_df = read_candidate_csv(f)
     files = generate_json_files(folder_path, csv_df)
     for candidate in csv_df.index:
@@ -88,7 +132,7 @@ def update_json_files(folder_path, sheet_url):
         )
         with open(path, "w") as f:
             json.dump(json_dict, f, indent=2)
-            f.write('\n')
+            f.write("\n")
 
 
 if __name__ == "__main__":
