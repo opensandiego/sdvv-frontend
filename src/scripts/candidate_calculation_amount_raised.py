2622ad8233374817cfee3b3d240c5cfb74089989
Bolun Thompson <abolunthompson@gmail.com>
2020-07-20 09:06:14 -0700

    Added documentation and refactored to make functions clearer.

--- src/scripts/candidate_calculation_amount_raised.py
+++ src/scripts/candidate_calculation_amount_raised.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Calculates the amount raised by each candidate by summing contributions
+from column `Tran_Amt1` in the CSV files and stores it in the
+candidate JSON files under field `file["raised vs spent"][0]["Raised"]`.
+"""
 
 from shared_calculations import (
     CONTRIBUTION_TYPE,
@@ -9,5 +14,5 @@ from shared_calculations import (
 
 if __name__ == "__main__":
     to_raised_json(
-        summed_contributions(CSV_PATHS, "Tran_Amt1", CONTRIBUTION_TYPE), "Raised",
+        summed_contributions(CSV_PATHS, CONTRIBUTION_TYPE, "Tran_Amt1"), "Raised",
     )
--- src/scripts/candidate_calculation_amount_spent.py
+++ src/scripts/candidate_calculation_amount_spent.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Calculates the amount spent by each candidate by summing expenditures
+from column `Calculated_Amount` in the CSV files and stores it in the
+candidate JSON files under field `file["raised vs spent"][0]["Spent"]`.
+"""
 
 from shared_calculations import (
     CSV_PATHS,
@@ -9,5 +14,5 @@ from shared_calculations import (
 
 if __name__ == "__main__":
     to_raised_json(
-        summed_contributions(CSV_PATHS, "Calculated_Amount", EXPENDITURE_TYPE), "Spent",
+        summed_contributions(CSV_PATHS, EXPENDITURE_TYPE, "Calculated_Amount"), "Spent",
     )
