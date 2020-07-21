db5b583ddfe57ef9759dc52e1c80251d43a7f6aa
gSoD17 <galentro451@gmail.com>
2020-07-20 18:32:40 -0700

    forgot to add sort order change for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import {
   MatButtonModule,
   MatExpansionModule,
@@ -15,18 +8,24 @@ import {
   MatToolbarModule,
   MatTooltipModule,
 } from '@angular/material';
-import { ChartsModule } from 'ng2-charts';
-import { HomeComponent } from './components/home/home.component';
-import { SplashComponent } from './components/splash/splash.component';
-import { MayorComponent } from './components/mayor/mayor.component';
-import { CityAttorneyComponent } from './components/city-attorney/city-attorney.component';
-import { CityCouncilComponent } from './components/city-council/city-council.component';
-import { UnderConstructionComponent } from './components/under-construction/under-construction.component';
-import { DistrictOneComponent } from './components/city-council/district-one/district-one.component';
-import { DistrictTwoComponent } from './components/city-council/district-two/district-two.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { CandidateCardComponent } from './components/candidate-card/candidate-card.component';
 import { CandidateCardExpandedComponent } from './components/candidate-card-expanded/candidate-card-expanded.component';
+import { ChartsModule } from 'ng2-charts';
+import { CityAttorneyComponent } from './components/city-attorney/city-attorney.component';
+import { CityCouncilComponent } from './components/city-council/city-council.component';
+import { DistrictOneComponent } from './components/city-council/district-one/district-one.component';
+import { DistrictTwoComponent } from './components/city-council/district-two/district-two.component';
 import { FaqComponent } from './components/faq/faq.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MayorComponent } from './components/mayor/mayor.component';
+import { NgModule } from '@angular/core';
+import { SplashComponent } from './components/splash/splash.component';
+import { UnderConstructionComponent } from './components/under-construction/under-construction.component';
 
 @NgModule({
   exports: [
