4e95e185561ca31f7b5c1299751ac479a544a02a
Robert <robertgz@users.noreply.github.com>
2020-08-11 18:12:50 -0700

    Added error checking for download_transactions

--- src/scripts/driver_script.js
+++ src/scripts/driver_script.js
@@ -1,9 +1,16 @@
 const {execFileSync} = require("child_process");
 
-// Run first since it updates/creates the JSON files and sets committee name to use as key in later py scripts.
-const download_info = execFileSync('python', ['calculation_download_gdrive_info.py']);
+try {
+  // This can take about 10 minutes to complete
+  const download_transactions = execFileSync('node', ['download_transactions_to_csv.js']);
+} catch(err) {
+  console.error(err);
+  return;
+}
 
-const download_transactions = execFileSync('node', ['download_transactions_to_csv.js']);
+// Run 'calculation_download_gdrive_info.py' first since it updates/creates the JSON files 
+// and sets committee name to use as key in later scripts.
+const download_info = execFileSync('python', ['calculation_download_gdrive_info.py']);
 
 const amount_raised = execFileSync('python', ['candidate_calculation_amount_raised.py']);
 const amount_spent = execFileSync('python', ['candidate_calculation_amount_spent.py']);
