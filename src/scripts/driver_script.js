1a10692cb3b6b8486fec3fc6b14d259466aef18c
Robert <robertgz@users.noreply.github.com>
2020-08-26 17:15:42 -0700

    Removed variable assignments

--- src/scripts/driver_script.js
+++ src/scripts/driver_script.js
@@ -2,7 +2,8 @@ const {execFileSync} = require("child_process");
 
 try {
   // This can take about 10 minutes to complete
-  const download_transactions = execFileSync('node', ['download_transactions_to_csv.js']);
+  execFileSync('node', ['download_transactions_to_csv.js']);
+
 } catch(err) {
   console.error(err);
   return;
@@ -10,12 +11,12 @@ try {
 
 // Run 'calculation_download_gdrive_info.py' first since it updates/creates the JSON files 
 // and sets committee name to use as key in later scripts.
-const download_info = execFileSync('python', ['calculation_download_gdrive_info.py']);
+execFileSync('python', ['calculation_download_gdrive_info.py']);
 
-const amount_raised = execFileSync('python', ['candidate_calculation_amount_raised.py']);
-const amount_spent = execFileSync('python', ['candidate_calculation_amount_spent.py']);
+execFileSync('python', ['candidate_calculation_amount_raised.py']);
+execFileSync('python', ['candidate_calculation_amount_spent.py']);
 
-const donor = execFileSync('python', ['candidate_calculation_donor.py']);
-const industry = execFileSync('python', ['candidate_calculation_industry.py']);
+execFileSync('python', ['candidate_calculation_donor.py']);
+execFileSync('python', ['candidate_calculation_industry.py']);
 
-const outside_spending = execFileSync('node', ['candidate_calc_outside_spending.js']);
+execFileSync('node', ['candidate_calc_outside_spending.js']);
