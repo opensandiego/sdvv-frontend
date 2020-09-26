ad47e7aac7cf1f4df1c96ed8b0eec0f2e8a9cff4
Bolun Thompson <abolunthompson@gmail.com>
2020-09-26 16:09:50 -0700

    Made candidate race sum calculation calculate the sum for each race
    Previously, the calculation candidate_race_sum_calculation.py calculated
    the sum of contributions for all races. This changes it so it calculates
    the sum for each individual race.

--- src/scripts/candidate_race_sum_calculation.py
+++ src/scripts/candidate_race_sum_calculation.py
@@ -1,36 +1,96 @@
 #!/usr/bin/env python3
 """
-Sums the contributions to all candidates and writes it to
-`campaign_race_totals.json` as a string.
+Sums the contributions for all candidates in each race
+(mayor, city council, city attorney) and writes it to
+`campaign_race_totals.json` as a JSON object.
+
+The keys are "mayor", "city council", and "city attorney" respectively.
+The key "last update" is the string date of when it was last updated,
+in MM/DD/YY format.
+
 The sum is rounded to the nearest whole number.
 """
 
-from shared_calculations import CONTRIBUTION_TYPE, CSV_PATHS, DIRECTORY, read_csv_series
+import datetime
+import glob
+import json
+import pathlib
+from typing import NamedTuple
+
+from shared_calculations import DIRECTORY
 
 
-def sum_candidate_contributions():
+class CandidateRaceContributionSums(NamedTuple):
     """
-    Sum of the contributions to each candidates.
+    The sums of the candidates' contributions for each race.
 
-    :returns: A rounded, integer sum of the contributions to all candidates
+    This also includes a last updated date.
     """
-    return int(round(read_csv_series(CSV_PATHS, CONTRIBUTION_TYPE, "Tran_Amt2").sum()))
+
+    mayor: int
+    city_council: int
+    city_attorney: int
+    last_update: datetime.date
 
 
-def to_json(contribution_sum, path=DIRECTORY + "campaign_race_totals.json"):
+def sum_race_contributions(
+    mayor_directory=DIRECTORY + "mayor/",
+    city_council_directory=DIRECTORY + "city_council*/",
+    city_attorney_directory=DIRECTORY + "mayor/",
+):
     """
-    Write the sum of all contributions as a string to `campaign_race_totals.json`
+    Sums the contributions for each race.
 
-    :param contribution_sum: An integer sum of candidate contributions
+    :param mayor_directory: The directory the mayoral race candidate JSON
+    files are in. Supports taking a glob pattern for multiple directories.
 
-    :param path: The file name  the candidate JSON files are found
-    in. Defaults to file "campaign_race_totals.json" under constant `DIRECTORY`.
+    :param city_council_directory: The directory the city council race
+    candidate JSON files are in. Supports taking a glob pattern for
+    multiple directories.
+
+    :param city_attorney_directory: The directory the city attorney race
+    candidate JSON files are in. Supports taking a glob pattern for
+    multiple directories.
+
+    :returns: A `CandidateRaceContributionSums` object.
+    """
+    sums = []
+    for directory in (mayor_directory, city_council_directory, city_attorney_directory):
+        rounded_sum = 0
+        expanded_paths = glob.iglob(directory)
+        for path in expanded_paths:
+            for json_file_path in pathlib.Path(path).rglob("*.json"):
+                with open(json_file_path) as file:
+                    candidate_dict = json.load(file)
+                    if "raised vs spent" in candidate_dict:
+                        rounded_sum += int(
+                            candidate_dict["raised vs spent"][0]["Raised"]
+                        )
+        sums.append(rounded_sum)
+    return CandidateRaceContributionSums(*sums, datetime.date.today())
+
+
+def to_json(contribution_sums, path=DIRECTORY + "campaign_race_totals.json"):
+    """
+    Writes the race contributions to `campaign_race_totals.json`
+
+    :param contribution_sums: A CandidateRaceContributionSums object
+
+    :param path: The file name the candidate JSON files are found
+    in. Defaults to file `campaign_race_totals.json` under constant `DIRECTORY`.
 
     :returns: None.
     """
+    race_sums = {
+        "mayor": str(contribution_sums.mayor),
+        "city council": str(contribution_sums.city_council),
+        "city attorney": str(contribution_sums.city_attorney),
+        "last update": contribution_sums.last_update.strftime("%m/%d/%Y"),
+    }
     with open(path, "w") as file:
-        file.write(str(contribution_sum))
+        json.dump(race_sums, file, indent=2)
+        file.write("\n")
 
 
 if __name__ == "__main__":
-    to_json(sum_candidate_contributions())
+    to_json(sum_race_contributions())
