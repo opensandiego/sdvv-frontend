009dc803d1e1e4aea499009b62bca3d876b520c4
Bolun Thompson <abolunthompson@gmail.com>
2020-10-18 10:04:47 -0700

    Changes race sum calculations to be by election year.
    Previously, the sum of the contributions in a race was for every
    election year. This changes it so that the sum is calculated seperately
    for each year.

--- src/scripts/candidate_race_sum_calculation.py
+++ src/scripts/candidate_race_sum_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Sums the contributions for all candidates in each race
+Sums the contributions for all candidates in each race by election year
 (mayor, city council, city attorney) and writes it to
-`campaign_race_totals.json` as a JSON object.
+/src/assets/candidates/{year}/campaign_race_totals.json as a JSON object.
 
 The keys are "mayor", "city council", and "city attorney" respectively.
 The key "last update" is the string date of when it was last updated,
@@ -14,6 +14,7 @@ The sum is rounded to the nearest whole number.
 import datetime
 import glob
 import json
+import os
 import pathlib
 from typing import NamedTuple
 
@@ -33,63 +34,85 @@ class CandidateRaceContributionSums(NamedTuple):
     last_update: datetime.date
 
 
+def directories_under(path):
+    """ An iterator of the directories directly under `path` """
+    return (folder.name for folder in os.scandir(path) if folder.is_dir())
+
+
 def sum_race_contributions(
-    mayor_directory=DIRECTORY + "*/mayor/",
-    city_council_directory=DIRECTORY + "*/city_council*/",
-    city_attorney_directory=DIRECTORY + "*/city_attorney/",
+    base_directory=DIRECTORY,
+    mayor_directory="mayor",
+    city_council_directory="city_council*",
+    city_attorney_directory="city_attorney",
 ):
     """
-    Sums the contributions for each race.
+    Sums the contributions for each race by election year.
+
+    :param base_directory: The directory containing folders for each election year
+
+    These directories support taking a glob pattern for multiple directories.
+    These are under `base_directory` in the format
+    "base_directory/*/directory_parameter".
 
     :param mayor_directory: The directory the mayoral race candidate JSON
-    files are in. Supports taking a glob pattern for multiple directories.
+    files are in.
 
     :param city_council_directory: The directory the city council race
-    candidate JSON files are in. Supports taking a glob pattern for
-    multiple directories.
+    candidate JSON files are in.
 
     :param city_attorney_directory: The directory the city attorney race
-    candidate JSON files are in. Supports taking a glob pattern for
-    multiple directories.
+    candidate JSON files are in.
 
-    :returns: A `CandidateRaceContributionSums` object.
+    :returns: A dictionary with the string election year as the key and
+    a `CandidateRaceContributionSums` object as the value.
     """
-    sums = []
-    for directory in (mayor_directory, city_council_directory, city_attorney_directory):
-        rounded_sum = 0
-        expanded_paths = glob.iglob(directory)
-        for path in expanded_paths:
-            for json_file_path in pathlib.Path(path).rglob("*.json"):
-                with open(json_file_path) as file:
-                    candidate_dict = json.load(file)
-                    if "raised vs spent" in candidate_dict:
-                        rounded_sum += int(
-                            candidate_dict["raised vs spent"][0]["Raised"]
-                        )
-        sums.append(rounded_sum)
-    return CandidateRaceContributionSums(*sums, datetime.date.today())
+    year_sums = {}
+    for year in directories_under(base_directory):
+        sums = []
+        for directory in (
+            mayor_directory,
+            city_council_directory,
+            city_attorney_directory,
+        ):
+            rounded_sum = 0
+            expanded_paths = glob.iglob(f"{base_directory}/{year}/{directory}")
+            for path in expanded_paths:
+                for json_file_path in pathlib.Path(path).rglob("*.json"):
+                    with open(json_file_path) as file:
+                        candidate_dict = json.load(file)
+                        if "raised vs spent" in candidate_dict:
+                            rounded_sum += int(
+                                candidate_dict["raised vs spent"][0]["Raised"]
+                            )
+            sums.append(rounded_sum)
+        year_sums[year] = CandidateRaceContributionSums(*sums, datetime.date.today())
+
+    return year_sums
 
 
-def to_json(contribution_sums, path=DIRECTORY + "campaign_race_totals.json"):
+def to_json(contribution_sums, path=DIRECTORY, file_name="campaign_race_totals.json"):
     """
     Writes the race contributions to `campaign_race_totals.json`
 
-    :param contribution_sums: A CandidateRaceContributionSums object
+    :param contribution_sums: A dictionary with keys of the election year
+    and values of the corrosponding `CandidateRaceContributionSums` object.
 
-    :param path: The file name the candidate JSON files are found
-    in. Defaults to file `campaign_race_totals.json` under constant `DIRECTORY`.
+    :param path: The path that contains folders for each election year.
+
+    :param file_name: The name of the output file
 
     :returns: None.
     """
-    race_sums = {
-        "mayor": str(contribution_sums.mayor),
-        "city council": str(contribution_sums.city_council),
-        "city attorney": str(contribution_sums.city_attorney),
-        "last update": contribution_sums.last_update.strftime("%m/%d/%Y"),
-    }
-    with open(path, "w") as file:
-        json.dump(race_sums, file, indent=2)
-        file.write("\n")
+    for year, contribution_sums in contribution_sums.items():
+        race_sums = {
+            "mayor": str(contribution_sums.mayor),
+            "city council": str(contribution_sums.city_council),
+            "city attorney": str(contribution_sums.city_attorney),
+            "last update": contribution_sums.last_update.strftime("%m/%d/%Y"),
+        }
+        with open(f"{path}/{year}/{file_name}", "w") as file:
+            json.dump(race_sums, file, indent=2)
+            file.write("\n")
 
 
 if __name__ == "__main__":
