fc42ad716219d847e3cc38beec453ec3dee08e55
Robert <robertgz@users.noreply.github.com>
2020-10-22 17:52:29 -0700

    added sdvv-frontend_spec.js

---
+++ cypress/integration/sdvv-frontend_spec.js
@@ -0,0 +1,48 @@
+describe('Build - Smoke Test', () => {
+
+  const links = [
+    { text: 'FAQ', url: '/faq' },
+    { text: 'About', url: '/about' },
+  ]
+  
+  links.forEach( link => {
+    it(`Clicking ${link.text} navigates to new URL`, () => {
+      cy.visit('')
+      cy.contains(link.text).click()
+      cy.url().should('include', link.url)
+    })
+  })
+
+  //Don't open link, just verify that it is set up correctly
+  it('Verify Register to Vote Link', () => {
+    cy.visit('')
+    cy.contains('Register to Vote')
+      .should('have.attr', 'href')
+      .and('include', 'registertovote.ca.gov')
+  })
+
+  const candidates = [
+    { text: 'Mayor', url: '/mayor' },
+    { text: 'City Attorney', url: '/city-attorney' },
+    { text: 'City Council', url: '/city-council' },
+  ]
+
+  candidates.forEach( candidate => {
+    it(`Clicking ${candidate.text} Candidates`, () => {
+      cy.visit('')
+      cy.contains(candidate.text).click()
+      cy.url().should('include', candidate.url)
+    })
+  })
+
+  const cityCouncilDistricts = [ '1', '3', '5', '7', '9']
+
+  cityCouncilDistricts.forEach( cityCouncilDistrict => {
+    const url = `/city-council-district-${cityCouncilDistrict}`
+    it(`Visit District ${cityCouncilDistrict} Candidates URL`, () => {
+      cy.visit(url)
+      cy.url().should('include', url)
+    })
+  })
+
+})
