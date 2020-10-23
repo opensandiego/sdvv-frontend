01018fe074f2d787fd1dd06056cfdb23c7ee7bfa
Robert <robertgz@users.noreply.github.com>
2020-10-22 20:13:52 -0700

    Combined arrays, commented out City Council test

--- cypress/integration/sdvv-frontend_spec.js
+++ cypress/integration/sdvv-frontend_spec.js
@@ -3,8 +3,11 @@ describe('Build - Smoke Test', () => {
   const links = [
     { text: 'FAQ', url: '/faq' },
     { text: 'About', url: '/about' },
+    { text: 'Mayor', url: '/mayor' },
+    { text: 'City Attorney', url: '/city-attorney' },
+    // { text: 'City Council', url: '/city-council' },
   ]
-  
+
   links.forEach( link => {
     it(`Clicking ${link.text} navigates to new URL`, () => {
       cy.visit('')
@@ -13,26 +16,12 @@ describe('Build - Smoke Test', () => {
     })
   })
 
-  //Don't open link, just verify that it is set up correctly
+  // Don't open link, just verify that it is set up correctly
   it('Verify Register to Vote Link', () => {
     cy.visit('')
     cy.contains('Register to Vote')
       .should('have.attr', 'href')
       .and('include', 'registertovote.ca.gov')
-  })
-
-  const candidates = [
-    { text: 'Mayor', url: '/mayor' },
-    { text: 'City Attorney', url: '/city-attorney' },
-    { text: 'City Council', url: '/city-council' },
-  ]
-
-  candidates.forEach( candidate => {
-    it(`Clicking ${candidate.text} Candidates`, () => {
-      cy.visit('')
-      cy.contains(candidate.text).click()
-      cy.url().should('include', candidate.url)
-    })
   })
 
   const cityCouncilDistricts = [ '1', '3', '5', '7', '9']
