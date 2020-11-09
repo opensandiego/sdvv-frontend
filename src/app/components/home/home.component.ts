0f30dee0ed67ff728df055cfe87412148a950af6
ShrimpMantis <jmahanta001@gmail.com>
2020-11-08 22:03:21 -0800

    empty side nav items were being created, corrected that issue. And mayor would be highlighted by default on rendering the landing page, corrected this issue

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,13 +14,13 @@ export class HomeComponent implements OnInit {
   isExpanded: boolean = false;
   showSubmenu: boolean = false;
   panelOpenState: boolean = false;
-  officeStep: number = 0;
+  officeStep: number = -1;
   councilDistrictStep: number = -1;
   selectedCandidate: string;
 
   candidates: Record<string, CandidateTree>;
-  modifiedData:{}={};
-  sortedObj:{}={};
+  modifiedData: {} = {};
+  sortedObj: {} = {};
 
   @ViewChild('drawer') sidenav: MatDrawer;
 
@@ -32,20 +32,20 @@ export class HomeComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   onResize(event): void {
-      if (this.sidenav !== undefined) {
-          if (event.target.innerWidth <= 1000) {
-              this.sidenav.close();
-          } else {
-              this.sidenav.open();
-          }
+    if (this.sidenav !== undefined) {
+      if (event.target.innerWidth <= 1000) {
+        this.sidenav.close();
+      } else {
+        this.sidenav.open();
       }
+    }
   }
 
   ngOnInit() {
     this.candidateService.getAll().then(
       (all: Record<string, CandidateTree>) => {
         this.candidates = all;
-  
+
         this.massageCandidateData();
       }
     )
@@ -66,60 +66,64 @@ export class HomeComponent implements OnInit {
     this.sidenavService.emitChangeFromSidenav(candidateKey);
   }
 
-  massageCandidateData(){
+  // data is turned into a key value pair
+  //object like this
+  //{
+  //  mayor:{},
+  //  city-council:{},
+  //  city-attorney:{}
+  //}
+  massageCandidateData() {
 
-    this.modifiedData["city council"]={} as CandidateTree;
-    this.modifiedData["city council"]["title"]="City Council";
-    this.modifiedData["city council"]["candidates"]={};
-    
-    var candidateObj= this.candidates;
- 
-    const entries=Object.entries(this.candidates);
+    this.modifiedData["city council"] = {} as CandidateTree;
+    this.modifiedData["city council"]["title"] = "City Council";
+    this.modifiedData["city council"]["name"] = "City Council";
+    this.modifiedData["city council"]["deepTree"] = true;
+    this.modifiedData["city council"]["candidates"] = {};
 
-    entries.forEach(entry=>{
-      if(!entry["0"].toLowerCase().includes("city-council")){
-        let test= entry["0"];
-        this.modifiedData[test]= {} as CandidateTree ;
-        this.modifiedData[entry["0"]]= entry["1"];
-      }else{
-        entry["1"]["name"]= entry["1"].title.slice(15);
-        let hyphenatedString=this.createTitle(entry);
-        entry["1"].title=  hyphenatedString.replace(/\s/g, '').toLowerCase().trim();
-       
-        this.modifiedData["city council"]["candidates"][entry["0"]]=entry["1"];
+    const entries = Object.entries(this.candidates);
+
+    entries.forEach(entry => {
+      if (!entry["0"].toLowerCase().includes("last")) {
+        if (!entry["0"].toLowerCase().includes("city-council")) {
+          this.modifiedData[entry["0"]] = entry["1"];
+          this.modifiedData[entry["0"]]["deeptree"]= false;
+        } else {
+          this.modifiedData["city council"]["candidates"][entry["0"]] = entry["1"];
+        }
       }
 
     });
 
-    this.sortedObj=this.sortObj(this.modifiedData);
+    this.sortedObj = this.sortObj(this.modifiedData);
   }
 
-  sortObj(modifiedObject){
-    let temp={};
-    let modData= this.modifiedData;
-     var sortedEntries= Object.keys(modifiedObject).sort(function(a, b){
-         return b.charCodeAt(0)-a.charCodeAt(0);
+
+  private sortObj(modifiedObject) {
+    let temp = {};
+    let modData = this.modifiedData;
+    var sortedEntries = Object.keys(modifiedObject).sort(function (a, b) {
+      return b.charCodeAt(0) - a.charCodeAt(0);
     });
-   
-    sortedEntries.forEach(x=>{
-       temp[x]= modData[x];
+
+    sortedEntries.forEach(x => {
+      temp[x] = modData[x];
     });
 
     return temp;
-        
+
   }
 
-  private createTitle(entry:[string, CandidateTree]){
-    let stringOne=entry["1"].title.replace(' ' ,'-').slice(0,22);
-    let stringTwo= entry["1"].title.slice(24);
-    let hyphenatedString= `${stringOne}-${stringTwo}`;
-    return hyphenatedString;
-  }
-  otherOfficeClicked(office:string){
-    // 
-  }
-  
-  asIsOrder(a,b){
+
+  // private createTitle(entry: [string, CandidateTree]) {
+  //   let stringOne = entry["1"].title.replace(' ', '-').slice(0, 22);
+  //   let stringTwo = entry["1"].title.slice(24);
+  //   let hyphenatedString = `${stringOne}-${stringTwo}`;
+  //   console.log("hyphenatedString", hyphenatedString);
+  //   return hyphenatedString.replace(/\s/g, '').toLowerCase().trim();
+  // }
+
+  asIsOrder(a, b) {
     return 1;
   }
 }
