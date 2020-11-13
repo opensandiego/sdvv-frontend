2181bf4091bdbfcd7b63457edbb79eaf336bd296
Matt <matthew9510@gmail.com>
2020-11-12 23:45:49 -0800

    Updated splash component to utilize the campaign_race_totals.json values

--- src/app/components/splash/splash.component.ts
+++ src/app/components/splash/splash.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import campaignRaceTotals from "../../../assets/candidates/2020/campaign_race_totals.json";
+import { RoundCurrencyDisplayPipe } from "../../pipes/round-currency-display.pipe";
 
 @Component({
-  selector: 'app-splash',
-  templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.scss']
+  selector: "app-splash",
+  templateUrl: "./splash.component.html",
+  styleUrls: ["./splash.component.scss"],
 })
 export class SplashComponent implements OnInit {
+  mayorTotal: string;
+  cityCouncilTotal: string;
+  cityAttorneyTotal: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.mayorTotal = campaignRaceTotals.mayor;
+    this.cityCouncilTotal = campaignRaceTotals["city council"];
+    this.cityAttorneyTotal = campaignRaceTotals["city attorney"];
   }
-
 }
