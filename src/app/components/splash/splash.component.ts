2e05579567bf737fba8dc0590a76999ef2bd5cce
Robert <robertgz@users.noreply.github.com>
2020-12-15 12:12:32 -0800

    included Campaign Totals in splash.component

--- src/app/components/splash/splash.component.ts
+++ src/app/components/splash/splash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidateService } from '../../services/candidate.service';
 
 @Component({
   selector: 'app-splash',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
 
-  constructor() { }
+  constructor(private candidateService: CandidateService) { }
+
+  campaignTotals = {
+    mayor: '0',
+    cityAttorney: '0',
+    cityCouncil: '0'
+  }
 
   ngOnInit() {
+    this.candidateService.getCampaignTotals()
+      .subscribe(totals => this.campaignTotals = totals);
   }
 
 }
