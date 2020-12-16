820b7f0694921d3a21d108c5afdd8de352b9d5a4
Robert <robertgz@users.noreply.github.com>
2020-12-15 16:11:45 -0800

    added number of candidates to splash component

--- src/app/services/candidate.service.ts
+++ src/app/services/candidate.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CandidateTree } from '../candidate';
 
 @Injectable({
@@ -18,6 +19,29 @@ export class CandidateService {
 
   getAll() {
     return this.http.get("assets/candidates/2020/candidates.json").toPromise();
+  }
+
+  getNumberOfCandidates(): Observable<any> {
+    const campaignCandidateTotalsFilePath = "assets/candidates/2020/campaign_candidate_totals.json";
+
+    const candidateCounts = this.http.get<any>(campaignCandidateTotalsFilePath)
+      .pipe( // transform the property names used from those in the json file
+        map(result => {
+          return {
+            'mayor': result['mayor'],
+            'cityAttorney': result['city attorney'],
+            'cityCouncil': result['city council']
+          }
+        })
+      );
+
+    return candidateCounts;
+  }
+
+  getNumberOfCandidatesByOffice(office: string): Observable<any> {
+    return this.getNumberOfCandidates().pipe(
+      map(offices => offices[office])
+    )
   }
 
   // Mayoral Candidates
