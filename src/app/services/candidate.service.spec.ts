80c487a7896d5e518f22adf7d64432a981f5c962
Robert <robertgz@users.noreply.github.com>
2021-01-13 00:37:26 -0800

    Addded HttpClientTestingModule to imports

--- src/app/services/candidate.service.spec.ts
+++ src/app/services/candidate.service.spec.ts
@@ -1,9 +1,12 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CandidateService } from './candidate.service';
 
 describe('CandidateService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ],
+  }));
 
   it('should be created', () => {
     const service: CandidateService = TestBed.inject(CandidateService);
