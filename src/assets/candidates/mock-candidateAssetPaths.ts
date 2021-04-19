aba15eb441f327451c62246e5c021b6410733479
Robert <robertgz@users.noreply.github.com>
2021-04-18 22:11:00 -0700

    adds data that will at a later time be from an API

---
+++ src/assets/candidates/mock-candidateAssetPaths.ts
@@ -0,0 +1,74 @@
+import { CandidateAssetsPaths } from '../../app/interfaces/candidateAssetPaths'
+
+export const  candidateAssets: CandidateAssetsPaths[] = [
+  {
+    id: '2423694785',
+    calculationsPath: 'assets/candidates/2020/mayor/barbara_bry/barbara_bry.json',
+    imagePath: 'assets/candidates/2020/mayor/barbara_bry/barbara_bry.png',
+  },
+  {
+    id: '3321057457',
+    calculationsPath: 'assets/candidates/2020/mayor/todd_gloria/todd_gloria.json',
+    imagePath:        'assets/candidates/2020/mayor/todd_gloria/todd_gloria.png',
+  },
+  {
+    id: '138487776',
+    calculationsPath: 'assets/candidates/2020/city_attorney/cory_briggs/cory_briggs.json',
+    imagePath: '',
+  },
+  {
+    id: '2857364706',
+    calculationsPath: 'assets/candidates/2020/city_attorney/marra_w._elliot/marra_w._elliot.json',
+    imagePath: '',
+  },
+  {
+    id: '1381287194',
+    calculationsPath: 'assets/candidates/2020/city_council/district_1/joe_lacava/joe_lacava.json',
+    imagePath: '',
+  },
+  {
+    id: '556961640',
+    calculationsPath: 'assets/candidates/2020/city_council/district_1/will_moore/will_moore.json',
+    imagePath: '',
+  },
+  // {
+  //   id: '',
+  //   calculationsPath: 'assets/candidates/2020/city_council/district_3/toni_duran/toni_duran.json',
+  //   imagePath: '',
+  // },
+  {
+    id: '3087659774',
+    calculationsPath: 'assets/candidates/2020/city_council/district_3/stephen_whitburn/stephen_whitburn.json',
+    imagePath: '',
+  },
+  // {
+  //   id: '',
+  //   calculationsPath: 'assets/candidates/2020/city_council/district_5/joe_leventhal/joe_leventhal.json',
+  //   imagePath: '',
+  // },
+  // {
+  //   id: '',
+  //   calculationsPath: 'assets/candidates/2020/city_council/district_5/marni_von_wilpert/marni_von_wilpert.json',
+  //   imagePath: '',
+  // },
+  // {
+  //   id: '',
+  //   calculationsPath: 'assets/candidates/2020/city_council/district_7/raul_campillo/raul_campillo.json',
+  //   imagePath: '',
+  // },
+  // {
+  //   id: '',
+  //   calculationsPath: 'assets/candidates/2020/city_council/district_7/noli_zosa/noli_zosa.json',
+  //   imagePath: '',
+  // },
+  {
+    id: '1372856752',
+    calculationsPath: 'assets/candidates/2020/city_council/district_9/kelvin_h._barrios/kelvin_h._barrios.json',
+    imagePath: '',
+  },
+  {
+    id: '4044118288',
+    calculationsPath: 'assets/candidates/2020/city_council/district_9/sean_elo/sean_elo.json',
+    imagePath: '',
+  },
+];
