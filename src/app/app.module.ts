a1427bdc66d3e3353a7cd1a1ae89840e93c2bb56
Robert <robertgz@users.noreply.github.com>
2021-06-02 15:13:52 -0700

    removes mayor,city-attorney,city-council component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,15 +15,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CandidateCardComponent } from './components/candidate-card/candidate-card.component';
 import { CandidateCardExpandedComponent } from './components/candidate-card-expanded/candidate-card-expanded.component';
 import { ChartsModule } from 'ng2-charts';
-import { CityAttorneyComponent } from './components/city-attorney/city-attorney.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MayorComponent } from './components/mayor/mayor.component';
 import { NgModule } from '@angular/core';
 import { SplashComponent } from './components/splash/splash.component';
 import { UnderConstructionComponent } from './components/under-construction/under-construction.component';
-import { CityCouncilDistrictComponent } from './components/city-council-district/city-council-district.component';
 import { RoundCurrencyDisplayPipe } from './pipes/round-currency-display.pipe';
 import { VvChartsModule } from './vv-charts/vv-charts.module';
 
@@ -39,13 +36,10 @@ import { OfficeComponent } from './components/office/office.component';
     HomeComponent,
     SplashComponent,
     FaqComponent,
-    MayorComponent,
-    CityAttorneyComponent,
     UnderConstructionComponent,
     CandidateCardComponent,
     CandidateCardExpandedComponent,
     AboutComponent,
-    CityCouncilDistrictComponent,
     RoundCurrencyDisplayPipe,
     CandidateNavigationComponent,
     OfficeComponent,
