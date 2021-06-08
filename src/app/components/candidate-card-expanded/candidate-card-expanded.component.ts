891b354cca45a5c45611ccbedd7c0c5f41727e25
Gino <galentro451@gmail.com>
2021-06-08 02:30:33 -0700

    refactor: removing unused chart code and related dependency functions in the expanded card ts

--- src/app/components/candidate-card-expanded/candidate-card-expanded.component.ts
+++ src/app/components/candidate-card-expanded/candidate-card-expanded.component.ts
@@ -17,107 +17,6 @@ export class CandidateCardExpandedComponent {
   public _c: Candidate;
   faQuestionCircle = faQuestionCircle;
   faTimesCircle = faTimesCircle;
-  // All Charts
-  chartPlugins = [pluginDataLabels];
-
-  // Oppose v. Support Chart
-  stackedHorizontalBarChartType: ChartType = 'horizontalBar';
-  stackedHorizontalBarChartData: ChartDataSets[] = [
-    {
-      data: [200000],
-      barPercentage: 0.4,
-      categoryPercentage: 1.0,
-      backgroundColor: 'rgba(0, 119, 255, 0.8)',
-      borderColor: 'rgba(0, 119, 255, 1)',
-      borderWidth:1,
-      hoverBackgroundColor:'rgba(0, 119, 255, 1)',
-      hoverBorderColor:'rgba(0, 119, 255, 1)',
-
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-        textAlign: 'left',
-        color: '#ffffff',
-
-
-        font: {
-          size: 16,
-          weight: 'bold',
-        },
-        
-        formatter: (val) => `Support\n$${this.mNumberFormatter(val)}`,
-      },
-    },
-
-    {
-      data: [5000],
-      barPercentage: 0.4,
-      categoryPercentage: 1.0,
-      backgroundColor: 'rgba(255, 113, 25, 0.8)',
-      borderColor: 'rgba(255, 113, 25, 1)',
-      borderWidth:1,
-      hoverBackgroundColor:'rgba(255, 113, 25, 1)',
-      hoverBorderColor:'rgba(255, 113, 25, 1)',
-
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-        textAlign: 'left',
-        color: '#ffffff',
-
-        font: {
-          size: 16,
-          weight: 'bold',
-        },
-
-        formatter: (val) => `Oppose\n$${this.mNumberFormatter(val)}`,
-      },
-    },
-   
-  ];
-
-
-  stackedHorizontalBarChartOptions: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-
-    layout: {
-      padding: {
-        left: 0,
-        right: 20,
-      },
-    },
-
-    legend: {
-      display: false,
-    },
-
-    tooltips: {
-      enabled: false,
-    },
-
-    scales: {
-      xAxes: [{
-        display: false,
-        
-        ticks: {
-          suggestedMin: 0,
-          suggestedMax: 900000
-        }
-      }],
-
-      yAxes: [{
-        display: false,
-        gridLines: {
-          color: "#727272",
-        },
-        ticks: {
-          min: 0,
-        }
-
-      }],
-    },
-  }
 
   constructor() { }
 
@@ -127,17 +26,6 @@ export class CandidateCardExpandedComponent {
 
   @Input() set candidate(c: Candidate) {
     this._c = c;
-    // console.debug(c);
-    this.setChartsData(c);
-  }
-
-  // Setting Chart Data
-  setChartsData(c: Candidate) {
-    // Oppose v. Support
-    this.stackedHorizontalBarChartData = [
-      {...this.stackedHorizontalBarChartData[0], data: [this.currencyToNumber(c['support'])]},
-      {...this.stackedHorizontalBarChartData[1], data: [this.currencyToNumber(c['oppose'])]}
-    ];
   }
 
   // Convert Currency String to Number
@@ -145,23 +33,7 @@ export class CandidateCardExpandedComponent {
     return Number(currencyString.replace(/[^0-9\.-]+/g,""))
   }
 
-  // Adding K At The End of Values Over 9999 (i.e. 10K, 100K)
-  mNumberFormatter(num: number) {
-    return Math.abs(num) > 1e6 ? Math.sign(num)*((Math.round(num/1e4))/100) + 'M' : this.commaNumberFormatter(Math.sign(num)*Math.abs(num));
-  }
-
-  // Adding Comma Separators for Values Over 999
-  commaNumberFormatter(num: number) {
-    return num.toLocaleString('en');
-  }
-
   close() {
     this.isExpanded.emit(false);
   }
-  
-  //convert string to int
-  ConvertToInt(val){
-    return parseInt(val);
-  }
-
 }
