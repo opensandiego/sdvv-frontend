0c22cf7382ed290eed8866148eeae2f3c38df4e4
Robert <robertgz@users.noreply.github.com>
2021-06-17 21:00:39 -0700

    changed the way the graphs is colored to be based on the value of the data

--- src/app/vv-charts/total-spent-donut/total-spent-donut.component.ts
+++ src/app/vv-charts/total-spent-donut/total-spent-donut.component.ts
@@ -13,27 +13,31 @@ import { RoundCurrencyPipe } from '../round-currency.pipe';
 export class TotalSpentDonutComponent implements OnChanges {
 
   @Input() spendingCategories: Object[];
-  
-  formatLabel(params) {
+
+  private chartColor = '#FF0000';
+
+  getFormattedLabel(params) {
     const decimalPlacesToRoundTo = 1;
     return params.percent.toFixed(decimalPlacesToRoundTo) + '%';
   }
 
-  formatTooltip(params) {
+  getFormattedTooltip(params) {
     return `<span style="font-size: 1em;">${params.data.name}<span><br />` 
          + `<span style="font-size: 1.5em;">${params.data.roundedAmount}<span>`;
   }
-  
+
+  getTooltipPosition(point, params, dom, rect, size) {
+    return [
+      (size.viewSize[0]/2) - size.contentSize[0]/2,
+      (size.viewSize[1]/2) - size.contentSize[1]/2,
+    ];
+  }
+
   chartOption: EChartsOption = {
     tooltip: {
       show: true,
-      position: function(point, params, dom, rect, size) {
-        return [
-          (size.viewSize[0]/2) - size.contentSize[0]/2,
-          (size.viewSize[1]/2) - size.contentSize[1]/2,
-        ];
-      },
-      formatter: this.formatTooltip,
+      position: this.getTooltipPosition,
+      formatter: this.getFormattedTooltip,
       backgroundColor: 'rgba(0, 0, 0, 0.0)',
       borderWidth: 0,
       shadowColor: 'rgba(0, 0, 0, 0.0)',
@@ -46,13 +50,22 @@ export class TotalSpentDonutComponent implements OnChanges {
     dataset: {
       // source: set in ngOnChanges
     },
+    visualMap: {
+      show: false,
+      // dimension: set in ngOnChanges
+      // min: set in ngOnChanges
+      // max: set in ngOnChanges
+      inRange: {
+        colorLightness: [.9, .3]
+     },
+    },
     series: [
       {
         type: 'pie',
         label: {
           show: true,
           position: 'inside',
-          formatter: this.formatLabel,
+          formatter: this.getFormattedLabel,
         },
         emphasis: {
           scaleSize: 10,
@@ -62,12 +75,12 @@ export class TotalSpentDonutComponent implements OnChanges {
             fontSize: '16',
           },
         },
-        radius: ['40%', '90%'],        
-
-        color: ['#830000', '#B40000', '#D30000', '#FF0000', '#FFADAD'].reverse(),
-
+        radius: ['40%', '90%'],
+        itemStyle: {
+          color: this.chartColor,
+        },
         encode: {
-          value: 'value', // references a parameter in dataset
+          // value: set in ngOnChanges
         },
       },
     ],
@@ -75,23 +88,47 @@ export class TotalSpentDonutComponent implements OnChanges {
 
   constructor(private currencyDisplayPipe: RoundCurrencyPipe) {  }
 
-  addRoundedAmounts(dataset) {
+  addRoundedAmounts(dataset, dataKeyName: string) {
     const decimalPlacesToRoundTo = 1;
 
-    return dataset.map( dataItem => ({ 
+    return dataset.map( dataItem => ({
         name: dataItem.name,
-        value: dataItem.value,
+        [dataKeyName]: dataItem.value,
         roundedAmount: '$' + this.currencyDisplayPipe
-            .transform(dataItem.value, decimalPlacesToRoundTo, '10000')
+            .transform(dataItem.value, decimalPlacesToRoundTo, '10000'),
       })
     );
 
   }
 
-  ngOnChanges(): void {
-    const spendingCategories = this.addRoundedAmounts(this.spendingCategories);
+  setVisualMapRange(categories: object[], categoryName: string): void {
+    const values = categories.map( category => category[categoryName] );
 
-    this.chartOption.dataset['source'] = spendingCategories.slice(0, 4);
+    this.chartOption.visualMap['min'] = Math.min(...values);
+    this.chartOption.visualMap['max'] = Math.max(...values);
+  }
+
+  setVisualMapDimension(categories: object[], keyName: string) {
+    const keys = Object.keys(categories[0]);
+
+    this.chartOption.visualMap['dimension'] = keys.indexOf(keyName);
+  }
+
+  setChartData(dataset: object[], keyName: string):void {
+    this.chartOption.series[0].encode.value = keyName;
+    this.chartOption.dataset['source'] = dataset;
+  }
+
+  ngOnChanges(): void {
+    const encodeDimensionName = 'value';
+
+    const spendingCategories = this.addRoundedAmounts(this.spendingCategories, encodeDimensionName).slice(0, 4);
+
+    this.setChartData(spendingCategories, encodeDimensionName);
+
+    this.setVisualMapRange(spendingCategories, encodeDimensionName);
+
+    this.setVisualMapDimension(spendingCategories, encodeDimensionName);
   }
 
 }
