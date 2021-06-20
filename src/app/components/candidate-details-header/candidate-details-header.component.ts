68d55d8173bff4404d31b806f224879ba7f8cbef
Robert <robertgz@users.noreply.github.com>
2021-06-20 16:46:50 -0700

    Added CandidateDetailsHeaderComponent

---
+++ src/app/components/candidate-details-header/candidate-details-header.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+import { RoundCurrencyPipe } from '../../vv-charts/round-currency.pipe';
+
+@Component({
+  selector: 'app-candidate-details-header',
+  templateUrl: './candidate-details-header.component.html',
+  styleUrls: ['./candidate-details-header.component.scss']
+})
+export class CandidateDetailsHeaderComponent implements OnChanges {
+
+  @Input() imageUrl: string;
+  @Input() candidateName: string;
+  @Input() description: string;
+  @Input() website: string;
+  @Input() raised: number;
+  @Input() donors: number;
+  @Input() averageDonation: number;
+
+  public raisedAmount: string;
+  public donorsCount: string;
+  public averageDonationAmount: string;
+
+  constructor(
+    private roundCurrencyPipe: RoundCurrencyPipe
+  ) { }
+
+  ngOnChanges(): void {
+    const decimalPlacesToRoundTo = 0;
+    const decimalPrefixThreshold = 10000;
+
+    this.raisedAmount = this.roundCurrencyPipe
+      .transform(this.raised, decimalPlacesToRoundTo, decimalPrefixThreshold);
+    
+    this.donorsCount = this.roundCurrencyPipe
+      .transform(this.donors, decimalPlacesToRoundTo, decimalPrefixThreshold);
+
+    this.averageDonationAmount = this.roundCurrencyPipe
+      .transform(this.averageDonation, decimalPlacesToRoundTo, decimalPrefixThreshold);
+  }
+
+}
