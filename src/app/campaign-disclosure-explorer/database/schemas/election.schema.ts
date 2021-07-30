5fefe6f734b3789464bbc8208256412f166d0a33
Robert <robertgz@users.noreply.github.com>
2021-07-29 21:26:39 -0700

    changed quotes from single to double

--- src/app/campaign-disclosure-explorer/database/schemas/election.schema.ts
+++ src/app/campaign-disclosure-explorer/database/schemas/election.schema.ts
@@ -5,24 +5,24 @@ import {
 } from 'rxdb';
 
 export const electionSchema = {
-  'title': 'election schema',
-  'description': 'election schema',
-  'version': 0,
-  'type': 'object',
-  'properties': {
-    'election_date': {
-      'type': 'string',
+  "title": "election schema",
+  "description": "election schema",
+  "version": 0,
+  "type": "object",
+  "properties": {
+    "election_date": {
+      "type": "string",
     },
-    'election_id': {
-      'type': 'string',
-      'primary': true,
+    "election_id": {
+      "type": "string",
+      "primary": true,
     },
-    'election_type': {
-      'type': 'string',
+    "election_type": {
+      "type": "string",
     },
-    'internal': {
-      'type': 'boolean',
+    "internal": {
+      "type": "boolean",
     },
   },
-  'required': [],
+  "required": ["election_id"],
 }
