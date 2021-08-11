5207b8fa8c2c6207b69949a7fcf0f0884f59da32
Robert <robertgz@users.noreply.github.com>
2021-08-10 21:13:33 -0700

    added committee schema

---
+++ src/app/campaign-disclosure-explorer/database/schemas/committee.schema.ts
@@ -0,0 +1,22 @@
+export const committeeSchema = {
+  "title": "committee schema",
+  "description": "committee schema",
+  "version": 0,
+  "type": "object",
+  "required": [],
+  "properties": {
+    "entity_id": {
+      "type": "string",
+      "primary": true,
+    },
+    "entity_name": {
+      "type": "string",
+    },
+    "entity_name_lower": {
+      "type": "string",
+    },
+    "entity_type": {
+      "type": "string",
+    },
+  }
+}
