14a4c63badc0eb4ee4a3f534cec10a33e9451b10
Robert <robertgz@users.noreply.github.com>
2021-08-25 19:32:01 -0700

    updated CampaignBackend Service

--- src/app/campaign-disclosure-explorer/services/campaign-backend.service.ts
+++ src/app/campaign-disclosure-explorer/services/campaign-backend.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { from, Observable, of, throwError } from 'rxjs';
+import { forkJoin, from, Observable, of, throwError } from 'rxjs';
 import { catchError, map, mergeMap, retry } from 'rxjs/operators';
 import { Election } from '../models/election.interface';
-import { DatabaseService } from '../database/database.service';
-
-interface EFileElectionResponse {
-  data: Election[];
-}
 
 @Injectable({
   providedIn: 'root'
@@ -15,36 +10,23 @@ interface EFileElectionResponse {
 export class CampaignBackendService {
   constructor(
     private http: HttpClient, 
-    private databaseService: DatabaseService,
   ) { }
-
-  eFilUrl = 'https://efile.sandiego.gov/api/v1/public/campaign-search/election/list';
 
   serverUrl = 'http://localhost:3000';
   electionsRoute = `${this.serverUrl}/elections`
+  electionsBulkRoute = `${this.serverUrl}/elections/bulk`
 
-  getElections(): Observable<Election>  {
-    return this.http.get<Election>(this.electionsRoute);
+  // Elections
+  getElections(): Observable<Election[]>  {
+    return this.http.get<Election[]>(this.electionsRoute);
   }
 
-  getElectionsFromEFile(): Observable<Election[]> {
-    const url = this.eFilUrl;
-    return this.http.get<EFileElectionResponse>(url).pipe(
-      map(response => response.data),
-      // catchError(error => console.error(error))
-    );
+  postOneElectionToRemote(election: Election): Observable<Election> {
+    return this.http.post<Election>(this.electionsRoute, election);
   }
 
-  postElectionsToRemote(elections: Election[]): Observable<Election>  {
-    // console.log(elections)
-    return from(elections).pipe(
-      mergeMap(election => this.http.post<Election>(this.electionsRoute, election))
-    )
-  }
-
-  async saveElectionsToLocalDB(elections) { //: Election[]
-    // const db = await this.databaseService.getInstance();
-    // db.addItemsToCollection(elections, db.collections.elections, 'election_id')
+  postBulkElectionsToRemote(elections: Election[]): Observable<Election[]> {
+    return this.http.post<Election[]>(this.electionsBulkRoute, elections);
   }
 
 }
