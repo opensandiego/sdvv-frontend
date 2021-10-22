c83d10aabb70ecfbd9e73162843838388edc392c
Robert <robertgz@users.noreply.github.com>
2021-10-22 00:20:40 -0700

    Updated ExpandedQuickViewComponent to use CandidateQuickViewService.

--- src/app/candidate-quick-view/expanded-quick-view/expanded-quick-view.component.ts
+++ src/app/candidate-quick-view/expanded-quick-view/expanded-quick-view.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { CandidateDataService } from '../../services/candidate-data.service';
 import type { RaisedVsSpent } from '../../vv-charts/interfaces/raisedVsSpent';
 import type { DonationsByGroup } from '../../vv-charts/interfaces/donationsByGroup';
 import type { RaisedInOut } from '../../vv-charts/interfaces/raisedInOut';
 import type { OutsideMoney } from '../../vv-charts/interfaces/outsideMoney';
-import type { CandidateCard } from '../../interfaces/candidateCard';
 
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { CandidateQuickViewService } from 'src/app/store/services/candidate.quickview.service';
+import { CandidateQuickView } from 'src/app/store/interfaces/candidate.quickview';
 
 
 @Component({
@@ -21,7 +20,6 @@ export class ExpandedQuickViewComponent implements OnChanges {
   @Input() candidateId: string;
   @Output() isExpanded = new EventEmitter<boolean>();
 
-  public candidateCard: CandidateCard;
   public raisedVsSpentData: RaisedVsSpent;
   public donationsByGroupData: DonationsByGroup;
   public raisedInOutData: RaisedInOut;
@@ -30,42 +28,50 @@ export class ExpandedQuickViewComponent implements OnChanges {
   faTimesCircle = faTimesCircle;
 
   constructor(
-    private candidateDataService: CandidateDataService,
     private router: Router,
     private candidateQuickViewService: CandidateQuickViewService,
   ) { }
+  
+  private setQuickView(quickView: CandidateQuickView): void {
+    this.raisedVsSpentData = {
+      id: quickView.raisedVsSpent.id,
+      raised: parseInt(quickView.raisedVsSpent.raised),
+      spent: parseInt(quickView.raisedVsSpent.spent),
+      averageDonation: parseInt(quickView.raisedVsSpent.averageDonation),
+    };
+
+    this.donationsByGroupData = {
+      id: quickView.donationsByGroupData.id,
+      groups: quickView.donationsByGroupData.groups.map(group => ({
+        ...group,
+        amount: parseInt(group.amount),
+        percent: parseInt(group.percent),
+      }))
+    };
+    
+    this.raisedInOutData = {
+      ...quickView.raisedInOut,
+      inside: parseInt(quickView.raisedInOut.inside),
+      outside: parseInt(quickView.raisedInOut.outside),
+    };
+
+    this.outsideMoneyData = {
+      ...quickView.outsideMoney,
+      support: parseInt(quickView.outsideMoney.support),
+      oppose: parseInt(quickView.outsideMoney.oppose),
+      scale: quickView.outsideMoney.scale ? parseInt(quickView.outsideMoney.scale) : 1,
+    };
+  }
 
   ngOnChanges(changes: SimpleChanges): void  {
 
-    if (changes['candidateId']) {
+    if (changes['candidateId'] && changes['candidateId'].currentValue) {
       let candidateId = changes['candidateId'].currentValue;
 
       this.candidateQuickViewService.getCandidate(candidateId)
         .subscribe( quickView => {
-          console.log('quickView', quickView);
-
-          this.raisedVsSpentData = {
-            id: quickView.raisedVsSpent.id,
-            raised: parseInt(quickView.raisedVsSpent.raised),
-            spent: parseInt(quickView.raisedVsSpent.spent),
-            averageDonation: parseInt(quickView.raisedVsSpent.averageDonation),
-          }
+          this.setQuickView(quickView);
         });
-  
-      // this.candidateDataService.getRaisedVsSpentChart(candidateId)
-      //   .subscribe( results => this.raisedVsSpentData = results);
-
-      this.candidateDataService.getDonationsByGroupChart(candidateId)
-        .subscribe( results => this.donationsByGroupData = results);  
-
-      this.candidateDataService.getRaisedInOutChart(candidateId)
-        .subscribe( results => this.raisedInOutData = results);
-
-      this.candidateDataService.getOutsideMoneyChart(candidateId)
-        .subscribe( results => this.outsideMoneyData = results);
-
-      this.candidateDataService.getCandidateCard(candidateId)
-        .subscribe( results => this.candidateCard = results);
     }
   }
 
