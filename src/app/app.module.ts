90bde062362b849614bcb7e29be0fb63b46c2fc3
Robert <robertgz@users.noreply.github.com>
2021-10-22 17:24:24 -0700

    Removed Faq, About, and UnderConstruction components from AppModule. Replacements  are in PublicModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { AboutComponent } from './components/about/about.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,12 +14,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CandidateCardComponent } from './components/candidate-card/candidate-card.component';
 import { CandidateCardExpandedComponent } from './components/candidate-card-expanded/candidate-card-expanded.component';
 import { ChartsModule } from 'ng2-charts';
-import { FaqComponent } from './components/faq/faq.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { SplashComponent } from './components/splash/splash.component';
-import { UnderConstructionComponent } from './components/under-construction/under-construction.component';
 import { RoundCurrencyDisplayPipe } from './pipes/round-currency-display.pipe';
 import { VvChartsModule } from './vv-charts/vv-charts.module';
 import { CandidateQuickViewModule } from './candidate-quick-view/candidate-quick-view.module';
@@ -38,11 +35,8 @@ import { APIStoreModule } from './store/api.store.module';
     AppComponent,
     HomeComponent,
     SplashComponent,
-    FaqComponent,
-    UnderConstructionComponent,
     CandidateCardComponent,
     CandidateCardExpandedComponent,
-    AboutComponent,
     RoundCurrencyDisplayPipe,
     CandidateNavigationComponent,
     OfficeComponent,
@@ -66,6 +60,7 @@ import { APIStoreModule } from './store/api.store.module';
     CandidateQuickViewModule,
     CandidateModule,
     APIStoreModule,
+    PublicModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
