fdd502c6e712efb419a23e0c8dc1bea796acb9d5
Robert <robertgz@users.noreply.github.com>
2021-11-09 17:14:32 -0800

    Fix for CandidateCardComponent storybook file. Fix for ExpandedQuickViewComponent storybook file. Fix for CandidateDetailsHeaderComponent storybook file.

--- src/app/candidate-quick-view/expanded-quick-view/expanded-quick-view.component.stories.ts
+++ src/app/candidate-quick-view/expanded-quick-view/expanded-quick-view.component.stories.ts
@@ -2,26 +2,14 @@ import { Meta, Story } from '@storybook/angular/types-6-0';
 import { moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
-import { from } from 'rxjs';
 
 import { ExpandedQuickViewComponent } from './expanded-quick-view.component';
 
-import { CandidateModule } from '../../candidate/candidate.module';
-import { CandidateQuickViewModule } from '../../candidate-quick-view/candidate-quick-view.module';
-
-import { CandidateDataService } from '../../shared/services/candidate-data.service';
-
-import * as CandidateCardStories from 
-  '../../candidate/candidate-card/candidate-card.component.stories';
-import * as ExpandedRaisedVsSpentStories from 
-  '../../candidate-quick-view/expanded-raised-vs-spent/expanded-raised-vs-spent.component.stories'
-import * as ContributionsByGroupStories from 
-  '../../candidate-quick-view/contributions-by-group/contributions-by-group.component.stories'
-import * as ExpandedRaisedInVsOutStories from 
-  '../../candidate-quick-view/expanded-raised-in-vs-out/expanded-raised-in-vs-out.component.stories'
-import * as ExpandedOutsideMoneyStories from 
-  '../../candidate-quick-view/expanded-outside-money/expanded-outside-money.component.stories'
-
+import { CandidateQuickViewModule } from '../candidate-quick-view.module';
+import { CandidateQuickViewService } from 'src/app/store/services/candidate.quickview.service';
+import { MockCandidateQuickViewService } from './expanded-quick-view.component.stories.mock-candidate.service';
+import { MockCandidateService } from 'src/app/candidate/candidate-card/candidate-card.component.stories.mock-candidate.service';
+import { CandidateService } from 'src/app/store/services/candidate.service';
 
 export default {
   title: 'Expanded/Quick View',
@@ -33,31 +21,9 @@ export default {
       imports: [
         RouterTestingModule,
         CommonModule,
-        CandidateModule,
         CandidateQuickViewModule,
       ],
       providers: [
-
-        {
-          provide: CandidateDataService, useValue: {
-            getCandidateCard: (id) => {
-              return from( [CandidateCardStories.Default.args.candidateCard] )
-            },
-            getRaisedVsSpentChart: (id) => {
-              return from( [ExpandedRaisedVsSpentStories.Default.args] )
-            },
-            getDonationsByGroupChart: (id) => {
-              return from( [{groups: ContributionsByGroupStories.Default.args.contributionGroups}] )
-            },
-            getRaisedInOutChart: (id) => {
-              return from( [ExpandedRaisedInVsOutStories.Default.args] )
-            },
-            getOutsideMoneyChart: (id) => {
-              return from( [ExpandedOutsideMoneyStories.Default.args] )
-            },
-          }
-
-        }
       ],
     }),
   ],  
@@ -67,15 +33,20 @@ export default {
 } as Meta;
 
 const Template: Story<ExpandedQuickViewComponent> = (args: ExpandedQuickViewComponent) => ({
+  moduleMetadata: {
+    providers: [
+      { provide: CandidateQuickViewService, useClass: MockCandidateQuickViewService },
+      { provide: CandidateService, useClass: MockCandidateService },
+    ],
+    imports: [
+    ],
+    exports: [
+    ]
+  },
   props: args,
 })
 
 export const Default = Template.bind({});
 Default.args = {
-  candidateId: '',
-  // candidateCard: CandidateCardStories.Default.args.candidateCard,
-  // raisedVsSpentData: ExpandedRaisedVsSpentStories.Default.args,
-  // donationsByGroupData: ContributionsByGroupStories.Default.args.contributionGroups,
-  // raisedInOutData: ExpandedRaisedInVsOutStories.Default.args,  
-  // outsideMoneyData: ExpandedOutsideMoneyStories.Default.args,  
+  candidateId: 'mock|id',
 };
--- src/app/candidate/candidate-card/candidate-card.component.stories.ts
+++ src/app/candidate/candidate-card/candidate-card.component.stories.ts
@@ -1,9 +1,12 @@
 import { Meta, Story } from '@storybook/angular/types-6-0';
 import { moduleMetadata } from '@storybook/angular';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CandidateCardComponent } from './candidate-card.component';
-
 import { CandidateModule } from '../../candidate/candidate.module';
+import { CandidateService } from 'src/app/store/services/candidate.service';
+import { MockCandidateService } from './candidate-card.component.stories.mock-candidate.service';
 
 export default {
   title: 'Candidate/Candidate Card',
@@ -22,34 +25,38 @@ export default {
   },
 } as Meta;
 
+const fakeActivatedRoute = {
+} as ActivatedRoute;
+
 const Template: Story<CandidateCardComponent> = (args: CandidateCardComponent) => ({
+  moduleMetadata: {
+    providers: [
+      { provide: CandidateService, useClass: MockCandidateService },
+      { provide: ActivatedRoute, useValue: fakeActivatedRoute },
+    ],
+    imports: [
+      RouterTestingModule,
+    ],
+    exports: [
+    ]
+  },
   props: args,
 })
 
 export const Default = Template.bind({});
 Default.args = {
-  candidateCard: {
-    id: '0',
-    name: 'Johnson Jones',
-    description: 'Place Holder for a Description',
-    raised: '150000',
-    donors: '3250',
-    candidateImgURL: 'assets/candidate-card/profile.png',
-  },
+  candidateId: '',
   inExpandedCard: true,
 };
 
 export const ExpandedCardView = Template.bind({});
 ExpandedCardView.args = {
-  candidateCard: {
-    ...Default.args.candidateCard,
-    website: 'https://opensandiego.org/',
-  },
+  candidateId: '',
   inExpandedCard: true,
 };
 
 export const OfficesListView = Template.bind({});
 OfficesListView.args = {
-  candidateCard: ExpandedCardView.args.candidateCard,
+  candidateId: '',
   inExpandedCard: false,
 };
