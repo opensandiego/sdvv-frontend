c71844ed0fbfad5db0ccdd05719fe612e285b393
Robert <robert@example.com>
2021-11-26 11:37:54 -0800

    Added years to mock office summary.

--- src/app/store/mocks/mock-office-summary-2020.ts
+++ src/app/store/mocks/mock-office-summary-2020.ts
@@ -2,22 +2,33 @@ import { OfficeSummary } from "src/app/store/interfaces/office.summary";
 
 export const officeSummaryItems: OfficeSummary[] = [
   {
-      "office": "Mayor",
-      "candidateCount": "7",
-      "year": "2020",
-      "totalRaised": "5562627"
+    "office": "City Council",
+    "candidateCount": "14",
+    "year": "2022",
+    "totalRaised": "999999"
   },
   {
-      "office": "City Council",
-      "candidateCount": "28",
-      "year": "2020",
-      "totalRaised": "3703579"
+    "office": "Mayor",
+    "candidateCount": "7",
+    "year": "2020",
+    "totalRaised": "5562627"
   },
   {
-      "office": "City Attorney",
-      "candidateCount": "3",
-      "year": "2020",
-      "totalRaised": "853088"
-  }
+    "office": "City Council",
+    "candidateCount": "28",
+    "year": "2020",
+    "totalRaised": "3703579"
+  },
+  {
+    "office": "City Attorney",
+    "candidateCount": "3",
+    "year": "2020",
+    "totalRaised": "853088"
+  },
+  {
+    "office": "City Council",
+    "candidateCount": "25",
+    "year": "2018",
+    "totalRaised": "999999"
+  },
 ];
-
