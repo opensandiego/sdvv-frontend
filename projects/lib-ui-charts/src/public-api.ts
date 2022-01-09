d1f3ec1b5ed35403ea76ce483caf4674b8d9008c
Robert <robertgz@users.noreply.github.com>
2022-01-09 15:21:37 -0800

    Added RaisedVsSpentBarModule to lib-ui-charts.

--- projects/lib-ui-charts/src/public-api.ts
+++ projects/lib-ui-charts/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/lib-ui-charts.service';
 export * from './lib/lib-ui-charts.component';
 export * from './lib/lib-ui-charts.module';
+
+export * from './lib/raised-vs-spent-bar/raised-vs-spent-bar.component'
+export * from './lib/raised-vs-spent-bar/raised-vs-spent-bar.module'
