f1fb6bf7e08291fbd43dfb22348f6a15efa435f5
Robert <robertgz@users.noreply.github.com>
2022-01-09 15:16:23 -0800

    Created library lib-ui-charts Ran ng generate library lib-ui-charts

---
+++ projects/lib-ui-charts/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of lib-ui-charts
+ */
+
+export * from './lib/lib-ui-charts.service';
+export * from './lib/lib-ui-charts.component';
+export * from './lib/lib-ui-charts.module';
