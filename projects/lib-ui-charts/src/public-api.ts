5633e69c701f40210371e7750c40643ce7cadfd4
Robert <robertgz@users.noreply.github.com>
2022-01-09 15:23:52 -0800

    Added TotalSpentDonutModule to lib-ui-charts.

--- projects/lib-ui-charts/src/public-api.ts
+++ projects/lib-ui-charts/src/public-api.ts
@@ -6,5 +6,8 @@ export * from './lib/lib-ui-charts.service';
 export * from './lib/lib-ui-charts.component';
 export * from './lib/lib-ui-charts.module';
 
+export * from './lib/total-spent-donut/total-spent-donut.component';
+export * from './lib/total-spent-donut/total-spent-donut.module';
+
 export * from './lib/raised-vs-spent-bar/raised-vs-spent-bar.component'
 export * from './lib/raised-vs-spent-bar/raised-vs-spent-bar.module'
