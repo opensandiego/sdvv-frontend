e731d4930f2fb7d645b3fc0d0b169bcf8e97cd64
Robert <robertgz@users.noreply.github.com>
2022-01-25 18:21:56 -0800

    Removed storybook version 6.4.9 Installed the current version 6.4.14 Included Example storybook stories.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,15 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docs: false,
-      }
-    },
+    "@storybook/addon-essentials"
   ],
-  features: {
-    postcss: false,
+  "framework": "@storybook/angular",
+  "core": {
+    "builder": "webpack5"
   },
+  "staticDirs": ['../src/assets'],
 }
