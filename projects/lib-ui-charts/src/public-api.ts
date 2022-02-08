ed03a2f28e2e7c90e3f9aa13f7c434603744091b
Robert <robertgz@users.noreply.github.com>
2022-02-07 16:48:46 -0800

    Added RaisedInVsOutDonutComponent to lib-ui-charts

--- projects/lib-ui-charts/src/public-api.ts
+++ projects/lib-ui-charts/src/public-api.ts
@@ -6,10 +6,13 @@ export * from './lib/lib-ui-charts.service';
 export * from './lib/lib-ui-charts.component';
 export * from './lib/lib-ui-charts.module';
 
-export * from './lib/total-spent-donut/total-spent-donut.component';
-export * from './lib/total-spent-donut/total-spent-donut.module';
+export * from './lib/raised-in-vs-out-donut/raised-in-vs-out-donut.component'
+export * from './lib/raised-in-vs-out-donut/raised-in-vs-out-donut.module'
 
 export * from './lib/raised-vs-spent-bar/raised-vs-spent-bar.component'
 export * from './lib/raised-vs-spent-bar/raised-vs-spent-bar.module'
+
+export * from './lib/total-spent-donut/total-spent-donut.component';
+export * from './lib/total-spent-donut/total-spent-donut.module';
 
 export * from './lib/lib-ui-charts.models'
