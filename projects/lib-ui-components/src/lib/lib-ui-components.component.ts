868f9da62e71809bece963ea7ebe3e41691635c4
Robert <robertgz@users.noreply.github.com>
2022-02-07 21:34:49 -0800

    Adds lib-ui-components

---
+++ projects/lib-ui-components/src/lib/lib-ui-components.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib-ui-components',
+  template: `
+    <p>
+      lib-ui-components works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibUiComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/lib-ui-components/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of lib-ui-components
+ */
+
+export * from './lib/lib-ui-components.service';
+export * from './lib/lib-ui-components.component';
+export * from './lib/lib-ui-components.module';
