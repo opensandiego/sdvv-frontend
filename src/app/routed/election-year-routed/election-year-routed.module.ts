d5c97fbc858af535a80dbb6c1f03461a153ec436
Robert <robertgz@users.noreply.github.com>
2022-02-13 12:49:25 -0800

    Moved ElectionYearRoutedModule into a folder

--- src/app/routed/election-year-routed/election-year-routed.module.ts
+++ src/app/routed/election-year-routed/election-year-routed.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { APIStoreModule } from '../store/api.store.module';
-import { YearSelectorGQLModule } from '../graphql/year-selector-gql/year-selector-gql.module';
+import { APIStoreModule } from '../../store/api.store.module';
+import { YearSelectorGQLModule } from '../../graphql/year-selector-gql/year-selector-gql.module';
 import { ElectionYearRouteComponent } from './election-year-routed.component';
 
 @NgModule({
