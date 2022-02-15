7d22b3a02cb78dd00d7aa02c58a3c43e7aa3c1e8
Robert <robertgz@users.noreply.github.com>
2022-02-15 00:23:20 -0800

    Updates build scripts

---
+++ src/environments/environment.db-staging.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://opensandiego-voters-v-staging.herokuapp.com',
+};
+
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://opensandiego-voters-v-staging.herokuapp.com'
+};
