eaf12e979d3ba3b80a320f8f4a543c25b56e7bf7
Robert <robertgz@users.noreply.github.com>
2022-03-13 16:04:08 -0700

    Adds YearCardListGQLComponent

---
+++ src/app/graphql/year-card-list-gql/year-card-list-gql.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { YearSelectorGQL, YearsResponse } from './year-card-list-gql.query';
+
+@Component({
+  selector: 'gql-year-card-list',
+  styleUrls: ['./year-card-list-gql.component.scss'],
+  template: `
+    <div class="list" *ngFor="let electionYear of electionYears">
+      <gql-year-card
+        [year]="electionYear"
+      ></gql-year-card>
+    </div>
+  `,
+})
+export class YearCardListGQLComponent implements OnInit {
+  electionYears: string[];
+
+  constructor(private yearSelectorGQL: YearSelectorGQL) {}
+
+  ngOnInit() {
+    this.yearSelectorGQL.watch({ }, {
+      // errorPolicy: 'all',
+    }).valueChanges.subscribe( (result: any) => {
+      const years: YearsResponse = result.data;
+
+      if (years.electionYears) {
+        this.electionYears = years.electionYears.map( electionYear => electionYear.year );
+      }
+    });
+  }
+
+}
