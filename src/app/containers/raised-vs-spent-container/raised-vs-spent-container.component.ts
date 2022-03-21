51201d3bd19e1c79dd6f9987b09f02b7566c641e
Robert <robertgz@users.noreply.github.com>
2022-03-20 22:22:36 -0700

    Adds RaisedVsSpentContainerModule

---
+++ src/app/containers/raised-vs-spent-container/raised-vs-spent-container.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, } from '@angular/core';
+
+@Component({
+  selector: 'raised-vs-spent-container',
+  templateUrl: './raised-vs-spent-container.component.html',
+  styleUrls: ['./raised-vs-spent-container.component.scss']
+})
+export class RaisedVsSpentContainerComponent {
+  @Input() candidateId: number;
+
+  title = 'Raised v. Spent';
+  tooltipText = 'Total campaign funds raised and spent by the candidate.';
+
+}
