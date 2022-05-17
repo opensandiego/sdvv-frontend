032b0a9b88bbd41114a5999a26a8a4af61cdfc66
Robert <robertgz@users.noreply.github.com>
2022-05-16 17:49:47 -0700

    Adds link preview to FAQ page

--- src/app/public/components/faq/faq.component.ts
+++ src/app/public/components/faq/faq.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 import { globals } from 'src/app/globals';
 
 @Component({
@@ -11,11 +11,19 @@ export class FaqComponent implements OnInit {
 
   constructor(
     private titleService: Title,
+    private metaService: Meta,
   ) { }
+
+  caption = 'Where is this data sourced? All of our data is sourced directly from the City of San Diego public records.';
 
   ngOnInit() {
     const pageTitle = `Faq | ${globals.pageTitleSuffix}`;
     this.titleService.setTitle(pageTitle);
+    this.metaService.addTags([
+      {property: 'og:url', content: location.href},
+      {property: 'og:title', content: pageTitle},
+      {property: 'og:description', content: this.caption},
+    ]);
   }
 
 }
