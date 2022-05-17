d483038b64a85453b7387a2d848b33eeea13d537
Robert <robertgz@users.noreply.github.com>
2022-05-16 17:49:16 -0700

    Adds link preview to About page

--- src/app/public/components/about/about.component.ts
+++ src/app/public/components/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 import { globals } from 'src/app/globals';
 
 @Component({
@@ -11,11 +11,20 @@ export class AboutComponent implements OnInit {
 
   constructor(
     private titleService: Title,
+    private metaService: Meta,
   ) { }
+
+  caption = 'We are a coalition of non-partisan organizations working together to create transparency for campaign contributions and expenditures with local San Diego candidates.';
 
   ngOnInit() {
     const pageTitle = `About | ${globals.pageTitleSuffix}`;
     this.titleService.setTitle(pageTitle);
+    this.metaService.addTags([
+      {property: 'og:url', content: location.href},
+      {property: 'og:title', content: pageTitle},
+      {property: 'og:image', content: `${location.origin}/assets/about/VV_FooterBG-AboutUs.png`},
+      {property: 'og:description', content: this.caption},
+    ]);
   }
 
 }
