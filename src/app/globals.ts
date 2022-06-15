b9f129cef4342cf696e599075058b9a1868ac8c2
Robert <robertgz@users.noreply.github.com>
2022-06-14 17:26:14 -0700

    Adds colors to globals for: contributions expendituresInSupport expendituresInOpposition

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,4 +1,10 @@
 
 export const globals = {
   pageTitleSuffix: 'San Diego Campaign Finance Transparency Dashboard',
+  contributionsColor: '#3392ff',
+  expendituresInSupportColor: '#289a58',
+  expendituresInSupportAltColor: '#207b46',
+  expendituresInSupportAlt2Color: '#53ae79',
+  expendituresInOppositionColor: '#ff2c19',
+  expendituresInOppositionAltColor: '#cc2314',
 }
