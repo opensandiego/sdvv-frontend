52e51a7661c03fd69e4f98d591dca31ca954f8f6
Robert <robertgz@users.noreply.github.com>
2022-08-28 15:17:05 -0700

    Updated HeaderComponent to use MatToolbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SideMenuService } from 'src/app/public/services/side-menu.service';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUniversity, faBalanceScale, faMapMarkedAlt, faQuestion, faBars, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import { SideMenuService } from 'src/app/public/services/side-menu.service';
 
 @Component({
   standalone: true,
   imports: [
     FontAwesomeModule,
     MatToolbarModule,
-    MatIconModule,
     RouterModule,
   ],
   selector: 'header',
@@ -23,7 +20,12 @@ import { faUniversity, faBalanceScale, faMapMarkedAlt, faQuestion, faBars, faChe
 export class HeaderComponent {
 
   homeLink = '/home';
-  public title = 'San Diego City - Campaign Finance Dashboard';
+
+  vvLogoURL = '../../../assets/home/VV_Logo_Reversed@2x.png';
+  vvAltText = `Voter's Voice Initiative`;
+  
+  public logoURL = this.vvLogoURL;
+  public logoAltText = this.vvAltText;
 
   constructor(
     public sideMenuService: SideMenuService,
