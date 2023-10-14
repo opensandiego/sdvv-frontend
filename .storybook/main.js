585bd94f8239257eb22a7bf388f7d69020f4bad9
Robert <robertgz@users.noreply.github.com>
2023-10-13 20:21:46 -0700

    Ran: npx storybook@latest upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)", "../projects/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "storybook-addon-angular-router"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-angular-router",
+    "@storybook/addon-mdx-gfm"
+  ],
   "framework": {
     name: "@storybook/angular",
     options: {}
