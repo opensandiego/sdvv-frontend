cee17ce2b003b26db57b5cfa8961d811ba6bbcad
Robert <robertgz@users.noreply.github.com>
2025-04-02 17:43:50 -0700

    Changed Storybook main file from js to ts. Updated config to work with TypeScript.

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  // Required
+  framework: '@storybook/angular',
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../projects/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  // Optional
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-angular-router',
+    '@storybook/addon-mdx-gfm',
+  ],
+  core: {
+    disableTelemetry: true,
+  },
+  staticDirs: ['../src/assets'],
+  docs: {},
+};
+
+export default config;
