42df0ce655d8b034bd18e194c94d0347a253a63c
Robert <robertgz@users.noreply.github.com>
2025-04-06 00:44:07 -0700

    Converted CandidateMenuComponent to standalone.
    Updated stories file.
    
    Updated  CandidateMenuModule to
    move component from declarations to imports. This module can be removed after upstream components are updated.
    
    This module had an import of FontAwesomeModule. After omitting it from the standalone component there were no errors when viewing in storybook.

--- projects/lib-ui-components/src/lib/candidate-menu/candidate-menu.component.ts
+++ projects/lib-ui-components/src/lib/candidate-menu/candidate-menu.component.ts
@@ -1,19 +1,22 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { PanelMenu } from 'primeng/panelmenu';
 import { ActiveMenuPath } from '../lib-ui-components.models';
 import { Office } from './office';
 
 @Component({
-    selector: 'candidate-menu',
-    template: `
+  selector: 'candidate-menu',
+  imports: [CommonModule, MenuModule, PanelMenu],
+  template: `
     <p-panelMenu
       [model]="items"
-      [style]="{'width':'200px'}"
+      [style]="{ width: '200px' }"
       [multiple]="false"
     ></p-panelMenu>
   `,
-    styleUrls: ['./candidate-menu.component.scss'],
-    standalone: false
+  styleUrls: ['./candidate-menu.component.scss'],
 })
 export class CandidateMenuComponent implements OnChanges {
   @Input() activeItem: ActiveMenuPath;
@@ -29,19 +32,18 @@ export class CandidateMenuComponent implements OnChanges {
 
   items: MenuItem[];
 
-  constructor( ) {
-    this.mayorOffice = 
-      new Office('Mayor', 'fa fa-fw fa-university');
-    this.cityCouncilOffice = 
-      new Office('City Council', 'fa fa-fw fa-map');
-    this.cityAttorneyOffice = 
-      new Office('City Attorney', 'fa fa-fw fa-balance-scale');
+  constructor() {
+    this.mayorOffice = new Office('Mayor', 'fa fa-fw fa-university');
+    this.cityCouncilOffice = new Office('City Council', 'fa fa-fw fa-map');
+    this.cityAttorneyOffice = new Office(
+      'City Attorney',
+      'fa fa-fw fa-balance-scale'
+    );
 
     this.renderMenu();
   }
 
   ngOnChanges(changes: SimpleChanges) {
-
     if (changes['detailsActive']) {
       this.detailsActive = changes['detailsActive'].currentValue;
       this.mayorOffice.addRouterLinks(this.detailsActive);
@@ -74,7 +76,10 @@ export class CandidateMenuComponent implements OnChanges {
   }
 
   changeOffice(office: Office, officeChange) {
-    if (officeChange?.currentValue && officeChange.currentValue?.candidates?.length > 0) {
+    if (
+      officeChange?.currentValue &&
+      officeChange.currentValue?.candidates?.length > 0
+    ) {
       const year = officeChange.currentValue?.electionYear;
       const candidates = officeChange.currentValue?.candidates;
 
@@ -92,6 +97,6 @@ export class CandidateMenuComponent implements OnChanges {
       this.mayorOffice.toMenuItem(this.activeItem),
       this.cityCouncilOffice.toMenuItem(this.activeItem),
       this.cityAttorneyOffice.toMenuItem(this.activeItem),
-    ]    
+    ];
   }
 }
