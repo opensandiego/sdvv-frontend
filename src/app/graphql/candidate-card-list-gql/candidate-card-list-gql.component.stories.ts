5d1412947ee7c260514e3327676ee962547a44e0
Robert <robertgz@users.noreply.github.com>
2025-04-06 14:56:10 -0700

    Converted CandidateCardListGQLComponent to standalone.
    Updated stories file.
    
    Updated  CandidateCardListGQLModule to
    move component from declarations to imports. This module can be removed after upstream components are updated.

--- src/app/graphql/candidate-card-list-gql/candidate-card-list-gql.component.stories.ts
+++ src/app/graphql/candidate-card-list-gql/candidate-card-list-gql.component.stories.ts
@@ -1,71 +1,63 @@
-import { applicationConfig, moduleMetadata } from '@storybook/angular';
-import { CommonModule } from '@angular/common';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
 import { GraphQLModule } from '../graphql.module';
 import { importProvidersFrom } from '@angular/core';
 
 import { CandidateCardListGQLComponent } from './candidate-card-list-gql.component';
-import { CandidateCardListGQLModule } from './candidate-card-list-gql.module';
 
-export default {
+const meta: Meta<CandidateCardListGQLComponent> = {
   title: 'Lib-gql/Candidate Card List',
   component: CandidateCardListGQLComponent,
   decorators: [
     applicationConfig({
       providers: [importProvidersFrom(GraphQLModule)],
     }),
-    moduleMetadata({
-      declarations: [],
-      imports: [
-        CommonModule,
-        CandidateCardListGQLModule,
-      ],
-      providers: [],
-    }),
   ],
 };
 
-export const Default = () => ({
-})
+export default meta;
+type Story = StoryObj<CandidateCardListGQLComponent>;
 
-export const Year2020 = () => ({
-  props: {
+export const Default: Story = {};
+
+export const Year2020: Story = {
+  args: {
     year: '2020',
   },
-})
+};
 
-export const Year2020Mayor = () => ({
-  props: {
+export const Year2020Mayor: Story = {
+  args: {
     year: '2020',
     office: 'Mayor',
   },
-})
+};
 
-export const Year2020CityAttorney = () => ({
-  props: {
+export const Year2020CityAttorney: Story = {
+  args: {
     year: '2020',
     office: 'City Attorney',
   },
-})
+};
 
-export const Year2020CityCouncil = () => ({
-  props: {
+export const Year2020CityCouncil: Story = {
+  args: {
     year: '2020',
     office: 'City Council',
   },
-})
+};
 
-export const Year2020CityCouncilDist1 = () => ({
-  props: {
+export const Year2020CityCouncilDist1: Story = {
+  args: {
     year: '2020',
     office: 'City Council',
     district: '1',
   },
-})
+};
 
-export const Year2020CityCouncilDist3 = () => ({
-  props: {
+export const Year2020CityCouncilDist3: Story = {
+  args: {
     year: '2020',
     office: 'City Council',
     district: '3',
   },
-})
+};
--- src/app/graphql/candidate-card-list-gql/candidate-card-list-gql.module.ts
+++ src/app/graphql/candidate-card-list-gql/candidate-card-list-gql.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
-import { GraphQLModule } from '../graphql.module';
-import { CandidateCardGQLModule } from '../candidate-card-gql/candidate-card-gql.module';
-import { CandidateCardListInfoGQL } from './candidate-card-list-gql.query';
 import { CandidateCardListGQLComponent } from './candidate-card-list-gql.component';
 
-@NgModule({ declarations: [
-        CandidateCardListGQLComponent,
-    ],
-    exports: [CandidateCardListGQLComponent],
-    bootstrap: [], imports: [CommonModule,
-        GraphQLModule,
-        CandidateCardGQLModule], providers: [CandidateCardListInfoGQL, provideHttpClient(withInterceptorsFromDi())] })
-export class CandidateCardListGQLModule { }
+@NgModule({
+  declarations: [],
+  exports: [CandidateCardListGQLComponent],
+  bootstrap: [],
+  imports: [CandidateCardListGQLComponent],
+  providers: [],
+})
+export class CandidateCardListGQLModule {}
