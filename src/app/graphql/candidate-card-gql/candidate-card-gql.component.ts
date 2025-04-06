22cd73de70b117df4ce22e31e0b7d59ff31f3027
Robert <robertgz@users.noreply.github.com>
2025-04-06 14:40:25 -0700

    Converted CandidateCardGQLComponent to standalone.
    Updated stories file.
    
    Updated  CandidateCardGQLModule to
    move component from declarations to imports. This module can be removed after upstream components are updated.

--- src/app/graphql/candidate-card-gql/candidate-card-gql.component.ts
+++ src/app/graphql/candidate-card-gql/candidate-card-gql.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-import { CandidateInfo, CommitteeData } from 'lib-ui-components';
+import { GraphQLModule } from '../graphql.module';
+import { CandidateCardModule, CandidateInfo, CommitteeData } from 'lib-ui-components';
 import { CandidateCardFinanceDataGQL, CandidateCardFinanceDataResponse } from './candidate-card-finance-data-gql.query';
 import { CandidateCardInfoGQL, CandidateCardInfoResponse } from './candidate-card-info-gql.query';
 import { globals } from 'src/app/globals';
@@ -9,9 +10,10 @@ import { globals } from 'src/app/globals';
 const uri = `${environment.apiUrl}`;
 
 @Component({
-    selector: 'gql-candidate-card',
-    template: `
-    <candidate-card 
+  selector: 'gql-candidate-card',
+  imports: [GraphQLModule, CandidateCardModule],
+  template: `
+    <candidate-card
       [candidateInfo]="candidateInfo"
       [committeeData]="committeeData"
       [inExpandedCard]="inExpandedCard"
@@ -19,7 +21,6 @@ const uri = `${environment.apiUrl}`;
       [inOppositionTextColor]="inOppositionTextColor"
     ></candidate-card>
   `,
-    standalone: false
 })
 export class CandidateCardGQLComponent implements OnChanges {
   @Input() candidateId: string;
@@ -33,10 +34,10 @@ export class CandidateCardGQLComponent implements OnChanges {
 
   constructor(
     private candidateInfoGQL: CandidateCardInfoGQL,
-    private candidateCardFinanceDataGQL: CandidateCardFinanceDataGQL,
-    ) {}
+    private candidateCardFinanceDataGQL: CandidateCardFinanceDataGQL
+  ) {}
 
-  ngOnChanges(changes: SimpleChanges): void  {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['candidateId']) {
       const candidateId = changes['candidateId'].currentValue;
       this.update(candidateId);
@@ -45,54 +46,69 @@ export class CandidateCardGQLComponent implements OnChanges {
 
   update(candidateId: string) {
     this.candidateId = candidateId;
-    
-    if (!this.candidateId) { return; }
 
-    this.candidateInfoGQL.watch({
-      candidateId: this.candidateId,
-    }, {
-      // errorPolicy: 'all',
-    }).valueChanges.subscribe( (result: any) => {
-      const response: CandidateCardInfoResponse = result.data;
+    if (!this.candidateId) {
+      return;
+    }
 
-      const candidateInfo = response?.candidate 
-        ? {
-          ...response.candidate,
-          imageUrl: response.candidate?.imageUrl 
-            ? `${uri}/${response.candidate.imageUrl}` 
-            : null
-        } 
-        : null;
-      this.candidateInfo = candidateInfo ? candidateInfo : null;
+    this.candidateInfoGQL
+      .watch(
+        {
+          candidateId: this.candidateId,
+        },
+        {
+          // errorPolicy: 'all',
+        }
+      )
+      .valueChanges.subscribe((result: any) => {
+        const response: CandidateCardInfoResponse = result.data;
 
-    });
+        const candidateInfo = response?.candidate
+          ? {
+              ...response.candidate,
+              imageUrl: response.candidate?.imageUrl
+                ? `${uri}/${response.candidate.imageUrl}`
+                : null,
+            }
+          : null;
+        this.candidateInfo = candidateInfo ? candidateInfo : null;
+      });
 
-    this.candidateCardFinanceDataGQL.watch({
-      candidateId: this.candidateId,
-    }, {
-      // errorPolicy: 'all',
-    }).valueChanges.subscribe( (result: any) => {
-      const response: CandidateCardFinanceDataResponse = result.data;
-      const donors = response?.candidate?.committee?.contributions?.count;
-      const raised = response?.candidate?.committee?.contributions?.sum;
-      const expenses = response?.candidate?.committee?.expenses.sum;
-      const independentExpenditures = response?.candidate?.independentExpenditures?.sums
-      const independentExpendituresSupporting = independentExpenditures?.support;
-      const independentExpendituresOpposing = independentExpenditures?.oppose;
+    this.candidateCardFinanceDataGQL
+      .watch(
+        {
+          candidateId: this.candidateId,
+        },
+        {
+          // errorPolicy: 'all',
+        }
+      )
+      .valueChanges.subscribe((result: any) => {
+        const response: CandidateCardFinanceDataResponse = result.data;
+        const donors = response?.candidate?.committee?.contributions?.count;
+        const raised = response?.candidate?.committee?.contributions?.sum;
+        const expenses = response?.candidate?.committee?.expenses.sum;
+        const independentExpenditures =
+          response?.candidate?.independentExpenditures?.sums;
+        const independentExpendituresSupporting =
+          independentExpenditures?.support;
+        const independentExpendituresOpposing = independentExpenditures?.oppose;
 
-      const totalInSupport = 
-        (expenses ? expenses : 0) +
-        (independentExpendituresSupporting ? independentExpendituresSupporting : 0);
-      const totalInOpposition = 
-        (independentExpendituresOpposing ? independentExpendituresOpposing : 0);
+        const totalInSupport =
+          (expenses ? expenses : 0) +
+          (independentExpendituresSupporting
+            ? independentExpendituresSupporting
+            : 0);
+        const totalInOpposition = independentExpendituresOpposing
+          ? independentExpendituresOpposing
+          : 0;
 
-      this.committeeData = {
-        raised: raised ? raised : 0,
-        donors: donors ? donors : 0,
-        totalInSupport: totalInSupport,
-        totalInOpposition: totalInOpposition,
-      }
-    });
-
+        this.committeeData = {
+          raised: raised ? raised : 0,
+          donors: donors ? donors : 0,
+          totalInSupport: totalInSupport,
+          totalInOpposition: totalInOpposition,
+        };
+      });
   }
 }
--- src/app/graphql/candidate-card-gql/candidate-card-gql.module.ts
+++ src/app/graphql/candidate-card-gql/candidate-card-gql.module.ts
@@ -1,18 +1,12 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CandidateCardModule } from 'lib-ui-components';
 
-import { GraphQLModule } from '../graphql.module';
-import { CandidateCardFinanceDataGQL } from './candidate-card-finance-data-gql.query';
-import { CandidateCardInfoGQL } from './candidate-card-info-gql.query';
 import { CandidateCardGQLComponent } from './candidate-card-gql.component';
 
-@NgModule({ declarations: [
-        CandidateCardGQLComponent,
-    ],
-    exports: [CandidateCardGQLComponent],
-    bootstrap: [], imports: [CommonModule,
-        GraphQLModule,
-        CandidateCardModule], providers: [CandidateCardInfoGQL, CandidateCardFinanceDataGQL, provideHttpClient(withInterceptorsFromDi())] })
-export class CandidateCardGQLModule { }
+@NgModule({
+  bootstrap: [],
+  imports: [CandidateCardGQLComponent],
+  declarations: [],
+  providers: [],
+  exports: [CandidateCardGQLComponent],
+})
+export class CandidateCardGQLModule {}
