f5a4b932ae4c445c3fbbfaedb0e87dfe1800afdb
Robert <robertgz@users.noreply.github.com>
2025-04-06 15:18:26 -0700

    Converted CandidateSummaryContainerComponent to standalone.
    Updated stories file.
    
    Updated  CandidateSummaryContainerModule to
    move component from declarations to imports. This module can be removed after upstream components are updated.

--- src/app/pages/candidates/summary/candidate-summary/candidate-summary-container.component.stories.ts
+++ src/app/pages/candidates/summary/candidate-summary/candidate-summary-container.component.stories.ts
@@ -1,48 +1,42 @@
-// import { Meta, Story } from '@storybook/angular/types-6-0';
-import { moduleMetadata } from '@storybook/angular';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { CandidateSummaryContainerModule } from './candidate-summary-container.module';
+import { GraphQLModule } from 'src/app/graphql/graphql.module';
 import { CandidateSummaryContainerComponent } from './candidate-summary-container.component';
 
-export default {
+const meta: Meta<CandidateSummaryContainerComponent> = {
   title: 'Lib Containers/Candidate Summary',
   component: CandidateSummaryContainerComponent,
   decorators: [
-    moduleMetadata({
-      declarations: [
-      ],
-      imports: [
-        CommonModule,
-        BrowserAnimationsModule,
-        CandidateSummaryContainerModule,
-      ],
-      providers: [
-      ],
+    applicationConfig({
+      providers: [importProvidersFrom(GraphQLModule), provideAnimationsAsync()],
     }),
   ],
 };
 
-export const Default = () => ({
-  props: {
-    candidateId: ''
-  },
-})
-export const BarbaraBry2020 = () => ({
-  props: {
-    candidateId: 'be0a57fb-c0f0-bbd5-0d42-44a6560cbd21|2020'
-  },
-})
+export default meta;
+type Story = StoryObj<CandidateSummaryContainerComponent>;
 
-export const JenniferCampbell2022 = () => ({
-  props: {
-    candidateId: 'd3d1c6e7-0add-49f1-9b3d-e9289e7efcf6|2022'
+export const Default: Story = {
+  args: {
+    candidateId: '',
   },
-})
+};
+export const BarbaraBry2020: Story = {
+  args: {
+    candidateId: 'be0a57fb-c0f0-bbd5-0d42-44a6560cbd21|2020',
+  },
+};
 
-export const MonicaMontgomerySteppe2022 = () => ({
-  props: {
-    candidateId: '82cae978-49df-4a77-8793-0efffb7772b5|2022'
+export const JenniferCampbell2022: Story = {
+  args: {
+    candidateId: 'd3d1c6e7-0add-49f1-9b3d-e9289e7efcf6|2022',
   },
-})
+};
+
+export const MonicaMontgomerySteppe2022: Story = {
+  args: {
+    candidateId: '82cae978-49df-4a77-8793-0efffb7772b5|2022',
+  },
+};
--- src/app/pages/candidates/summary/candidate-summary/candidate-summary-container.component.ts
+++ src/app/pages/candidates/summary/candidate-summary/candidate-summary-container.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
+import { GraphQLModule } from 'src/app/graphql/graphql.module';
+import { CandidateCardGQLComponent } from 'src/app/graphql/candidate-card-gql/candidate-card-gql.component';
+import { RaisedVsSpentContainerModule } from '../raised-vs-spent-container/raised-vs-spent-container.module';
+import { ContributionsByOccupationContainerModule } from '../contributions-by-occupation-container/contributions-by-occupation-container.module';
+import { RaisedInVsOutContainerModule } from '../raised-in-vs-out-container/raised-in-vs-out-container.module';
+import { IndependentExpendituresV2ContainerComponent } from '../independent-expenditures-v2-container/independent-expenditures-v2-container.component';
+
 @Component({
-    selector: 'candidate-summary-container',
-    templateUrl: './candidate-summary-container.component.html',
-    styleUrls: ['./candidate-summary-container.component.scss'],
-    standalone: false
+  selector: 'candidate-summary-container',
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    RouterModule,
+
+    GraphQLModule,
+    CandidateCardGQLComponent,
+    RaisedVsSpentContainerModule,
+    ContributionsByOccupationContainerModule,
+    RaisedInVsOutContainerModule,
+    IndependentExpendituresV2ContainerComponent,
+  ],
+  templateUrl: './candidate-summary-container.component.html',
+  styleUrls: ['./candidate-summary-container.component.scss'],
 })
 export class CandidateSummaryContainerComponent {
   @Input() candidateId: string;
