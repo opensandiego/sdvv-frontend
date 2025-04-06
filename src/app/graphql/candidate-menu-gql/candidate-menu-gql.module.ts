2eecbfd270bf40297279815856d7ef352927977c
Robert <robertgz@users.noreply.github.com>
2025-04-06 15:33:13 -0700

    Converted CandidateMenuGQLComponent to standalone.
    Updated stories file.
    
    Updated  CandidateMenuGQLModule. This module can be removed after upstream components are updated.

--- src/app/graphql/candidate-menu-gql/candidate-menu-gql.module.ts
+++ src/app/graphql/candidate-menu-gql/candidate-menu-gql.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-import { CandidateMenuModule } from 'lib-ui-components';
-import { GraphQLModule } from '../graphql.module';
 import { CandidateMenuGQLComponent } from './candidate-menu-gql.component';
-import { CandidateMenuGQL } from './candidate-menu-gql.query';
 
-@NgModule({ declarations: [
-        CandidateMenuGQLComponent,
-    ],
-    exports: [CandidateMenuGQLComponent],
-    bootstrap: [], imports: [CommonModule,
-        GraphQLModule,
-        CandidateMenuModule], providers: [CandidateMenuGQL, provideHttpClient(withInterceptorsFromDi())] })
-export class CandidateMenuGQLModule { }
+@NgModule({
+  declarations: [],
+  exports: [CandidateMenuGQLComponent],
+  bootstrap: [],
+  imports: [CandidateMenuGQLComponent],
+})
+export class CandidateMenuGQLModule {}
