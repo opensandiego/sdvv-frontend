a3e3caba3cb0e8c86ec52eb921b54dfa8088b395
Robert <robertgz@users.noreply.github.com>
2025-04-07 15:25:08 -0700

    Converted YearSelectorGQLComponent to standalone.
    Updated stories file.
    
    Updated YearSelectorGQLModule. This module can be removed after upstream components are updated.

--- src/app/graphql/year-selector-gql/year-selector-gql.module.ts
+++ src/app/graphql/year-selector-gql/year-selector-gql.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
 
-import { YearSelectorComponent } from 'lib-ui-components';
-import { GraphQLModule } from '../graphql.module';
 import { YearSelectorGQLComponent } from './year-selector-gql.component';
-import { YearSelectorGQL } from './year-selector-gql.query';
 
 @NgModule({
-  declarations: [YearSelectorGQLComponent],
+  declarations: [],
   exports: [YearSelectorGQLComponent],
   bootstrap: [],
-  imports: [CommonModule, GraphQLModule, YearSelectorComponent],
-  providers: [YearSelectorGQL, provideHttpClient(withInterceptorsFromDi())],
+  imports: [YearSelectorGQLComponent],
 })
 export class YearSelectorGQLModule {}
