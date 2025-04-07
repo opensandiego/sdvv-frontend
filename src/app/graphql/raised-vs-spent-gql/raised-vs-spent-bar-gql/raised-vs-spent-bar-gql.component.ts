52a6c2f46922ebfc96011a93f6f451f053cd4cb1
Robert <robertgz@users.noreply.github.com>
2025-04-07 15:02:43 -0700

    Converted RaisedVsSpentBarGQLComponent, AverageDonationGQLComponent to standalone.
    Updated stories file.
    
    Updated  RaisedVsSpentGQLModule. This module can be removed after upstream components are updated.

--- src/app/graphql/raised-vs-spent-gql/raised-vs-spent-bar-gql/raised-vs-spent-bar-gql.component.ts
+++ src/app/graphql/raised-vs-spent-gql/raised-vs-spent-bar-gql/raised-vs-spent-bar-gql.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { RaisedVsSpentGQL, RaisedVsSpent } from './raised-vs-spent-gql.query';
 import { globals } from 'src/app/globals';
+import { RaisedVsSpentBarComponent } from 'lib-ui-charts';
+import { GraphQLModule } from '../../graphql.module';
 
 @Component({
-    selector: 'gql-raised-vs-spent',
-    template: `
+  selector: 'gql-raised-vs-spent',
+  imports: [GraphQLModule, RaisedVsSpentBarComponent],
+  template: `
     <raised-vs-spent-bar
       [raised]="raised"
       [spent]="spent"
@@ -12,7 +15,6 @@ import { globals } from 'src/app/globals';
       [spentBarColor]="expendituresInSupportColor"
     ></raised-vs-spent-bar>
   `,
-    standalone: false
 })
 export class RaisedVsSpentBarGQLComponent implements OnChanges {
   @Input() candidateId: string;
@@ -24,7 +26,7 @@ export class RaisedVsSpentBarGQLComponent implements OnChanges {
 
   constructor(private raisedVsSpentGQL: RaisedVsSpentGQL) {}
 
-  ngOnChanges(changes: SimpleChanges): void  {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['candidateId']) {
       const candidateId = changes['candidateId'].currentValue;
       this.update(candidateId);
@@ -34,20 +36,27 @@ export class RaisedVsSpentBarGQLComponent implements OnChanges {
   update(candidateId: string) {
     this.candidateId = candidateId;
 
-    if (!this.candidateId) { return; }
+    if (!this.candidateId) {
+      return;
+    }
 
-    this.raisedVsSpentGQL.watch({
-      candidateId: this.candidateId,
-    }, {
-      // errorPolicy: 'all',
-    }).valueChanges.subscribe( (result: any) => {
-      const response: RaisedVsSpent = result.data;
+    this.raisedVsSpentGQL
+      .watch(
+        {
+          candidateId: this.candidateId,
+        },
+        {
+          // errorPolicy: 'all',
+        }
+      )
+      .valueChanges.subscribe((result: any) => {
+        const response: RaisedVsSpent = result.data;
 
-      const raised = response?.candidate?.committee?.contributions?.sum;
-      this.raised = raised ? raised : 0;
+        const raised = response?.candidate?.committee?.contributions?.sum;
+        this.raised = raised ? raised : 0;
 
-      const spent = response?.candidate?.committee?.expenses?.sum;
-      this.spent = spent ? spent : 0;
-    });
+        const spent = response?.candidate?.committee?.expenses?.sum;
+        this.spent = spent ? spent : 0;
+      });
   }
 }
