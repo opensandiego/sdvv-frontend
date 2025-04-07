1a8ab201d95444eafdae88e922baa5c7e10d65ae
Robert <robertgz@users.noreply.github.com>
2025-04-06 23:49:16 -0700

    Converted DetailsContributionsByOccupationHeaderComponent to standalone.
    Updated stories file.
    
    Updated  DetailsContributionsByOccupationHeaderModule. This module can be removed after upstream components are updated.

--- projects/lib-ui-components/src/lib/details-contributions-by-occupation-header/details-contributions-by-occupation-header.module.ts
+++ projects/lib-ui-components/src/lib/details-contributions-by-occupation-header/details-contributions-by-occupation-header.module.ts
@@ -1,27 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSortAmountDownAlt, faSortAmountUp } from '@fortawesome/free-solid-svg-icons';
 
 import { DetailsContributionsByOccupationHeaderComponent } from './details-contributions-by-occupation-header.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    FontAwesomeModule,
-  ],
-  declarations: [
-    DetailsContributionsByOccupationHeaderComponent
-  ],
-  providers: [  ], 
-  exports: [ DetailsContributionsByOccupationHeaderComponent ],
-  bootstrap: []
+  bootstrap: [],
+  imports: [DetailsContributionsByOccupationHeaderComponent],
+  declarations: [],
+  providers: [],
+  exports: [DetailsContributionsByOccupationHeaderComponent],
 })
-export class DetailsContributionsByOccupationHeaderModule {
-  constructor(library: FaIconLibrary) {
-    // Add an icon to the library for access in components
-    library.addIcons(faSortAmountUp, faSortAmountDownAlt);
-  }
-}
+export class DetailsContributionsByOccupationHeaderModule {}
