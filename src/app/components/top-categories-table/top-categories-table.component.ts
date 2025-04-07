6ffceeca6ee71c4d77ad5e899c632c7653d5ccd3
Robert <robertgz@users.noreply.github.com>
2025-04-07 14:46:25 -0700

    Updated stories for TopCategoriesTableComponent. Reformatted code in TopCategoriesTableComponent.

--- src/app/components/top-categories-table/top-categories-table.component.ts
+++ src/app/components/top-categories-table/top-categories-table.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnChanges, Output, EventEmitter, OnInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+  OnInit,
+} from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { getCompactFormattedCurrency } from 'src/app/public/util/number-formatter';
 
@@ -11,23 +18,20 @@ export interface Category {
 }
 
 @Component({
-    imports: [
-        MatTableModule,
-    ],
-    selector: 'app-top-categories-table',
-    templateUrl: './top-categories-table.component.html',
-    styleUrls: ['./top-categories-table.component.scss']
+  imports: [MatTableModule],
+  selector: 'app-top-categories-table',
+  templateUrl: './top-categories-table.component.html',
+  styleUrls: ['./top-categories-table.component.scss'],
 })
 export class TopCategoriesTableComponent implements OnChanges, OnInit {
-
   @Input() categories: Category[];
   @Input() categoryHighlighted: string;
   @Output() categoryHighlightedChange = new EventEmitter<string>();
-    
+
   dataSource;
   displayedColumns: string[] = ['name', 'value', 'percent'];
 
-  constructor() { }
+  constructor() {}
   ngOnInit(): void {
     let categories = this.categories;
     this.setTabletData(categories);
@@ -39,20 +43,18 @@ export class TopCategoriesTableComponent implements OnChanges, OnInit {
   }
 
   setTabletData(tableData: object[]): void {
-
-    this.dataSource = tableData.map( (row) => ({
-        ...row,
-        value: getCompactFormattedCurrency(row['value']),
-        boxShadowStyle: `10px 0px 0px 0px ${row['color']} inset`,
-        percent: (row['percent']/100)
-          .toLocaleString("en", { style: "percent", minimumFractionDigits: 1 }),
-      })
-    );
-
+    this.dataSource = tableData.map((row) => ({
+      ...row,
+      value: getCompactFormattedCurrency(row['value']),
+      boxShadowStyle: `10px 0px 0px 0px ${row['color']} inset`,
+      percent: (row['percent'] / 100).toLocaleString('en', {
+        style: 'percent',
+        minimumFractionDigits: 1,
+      }),
+    }));
   }
 
-  hoveredRow(row: object) {  
+  hoveredRow(row: object) {
     this.categoryHighlightedChange.emit(row['id']);
   }
-
 }
