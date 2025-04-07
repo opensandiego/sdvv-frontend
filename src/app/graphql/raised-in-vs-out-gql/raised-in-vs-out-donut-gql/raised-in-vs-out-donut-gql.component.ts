c3b25e242bf7dbee202e4464d7eb6462cdc7947c
Robert <robertgz@users.noreply.github.com>
2025-04-07 14:57:11 -0700

    Converted RaisedInVsOutTitleGQLComponent, RaisedInVsOutDonutGQLComponent to standalone.
    Updated stories file.
    
    Updated  RaisedInVsOutGQLModule. This module can be removed after upstream components are updated.

--- src/app/graphql/raised-in-vs-out-gql/raised-in-vs-out-donut-gql/raised-in-vs-out-donut-gql.component.ts
+++ src/app/graphql/raised-in-vs-out-gql/raised-in-vs-out-donut-gql/raised-in-vs-out-donut-gql.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { RaisedInVsOutGQL, RaisedInVsOut } from './raised-in-vs-out-gql.query';
+import { GraphQLModule } from '../../graphql.module';
+import { RaisedInVsOutDonutComponent } from 'lib-ui-charts';
 
 @Component({
-    selector: 'gql-in-vs-out-donut',
-    template: `
+  selector: 'gql-in-vs-out-donut',
+  imports: [GraphQLModule, RaisedInVsOutDonutComponent],
+  template: `
     <raised-in-vs-out-donut
       [inside]="insideAmount"
       [outside]="outsideAmount"
     ></raised-in-vs-out-donut>
   `,
-    standalone: false
 })
 export class RaisedInVsOutDonutGQLComponent implements OnChanges {
   @Input() candidateId: string;
@@ -19,7 +21,7 @@ export class RaisedInVsOutDonutGQLComponent implements OnChanges {
 
   constructor(private raisedInVsOutGQL: RaisedInVsOutGQL) {}
 
-  ngOnChanges(changes: SimpleChanges): void  {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['candidateId']) {
       const candidateId = changes['candidateId'].currentValue;
       this.update(candidateId);
@@ -29,20 +31,31 @@ export class RaisedInVsOutDonutGQLComponent implements OnChanges {
   update(candidateId: string) {
     this.candidateId = candidateId;
 
-    if (!this.candidateId) { return; }
+    if (!this.candidateId) {
+      return;
+    }
 
-    this.raisedInVsOutGQL.watch({
-      candidateId: this.candidateId,
-    }, {
-      // errorPolicy: 'all',
-    }).valueChanges.subscribe( (result: any) => {
-      const response: RaisedInVsOut = result.data;
+    this.raisedInVsOutGQL
+      .watch(
+        {
+          candidateId: this.candidateId,
+        },
+        {
+          // errorPolicy: 'all',
+        }
+      )
+      .valueChanges.subscribe((result: any) => {
+        const response: RaisedInVsOut = result.data;
 
-      const insideAmount = response?.candidate?.committee?.contributions?.categorizedBy?.jurisdiction?.inside;
-      this.insideAmount = insideAmount ? insideAmount : 0;
+        const insideAmount =
+          response?.candidate?.committee?.contributions?.categorizedBy
+            ?.jurisdiction?.inside;
+        this.insideAmount = insideAmount ? insideAmount : 0;
 
-      const outsideAmount = response?.candidate?.committee?.contributions?.categorizedBy?.jurisdiction?.outside;
-      this.outsideAmount = outsideAmount ? outsideAmount : 0;
-    });
+        const outsideAmount =
+          response?.candidate?.committee?.contributions?.categorizedBy
+            ?.jurisdiction?.outside;
+        this.outsideAmount = outsideAmount ? outsideAmount : 0;
+      });
   }
 }
--- src/app/graphql/raised-in-vs-out-gql/raised-in-vs-out-title-gql/raised-in-vs-out-title-gql.component.ts
+++ src/app/graphql/raised-in-vs-out-gql/raised-in-vs-out-title-gql/raised-in-vs-out-title-gql.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CandidateInfoGQL, CandidateInfo } from './candidate-info-gql.query';
+import { ChartTitleComponent } from 'lib-ui-components';
+import { GraphQLModule } from '../../graphql.module';
 
 @Component({
-    selector: 'gql-raised-in-vs-out-title',
-    template: `
-    <chart-title class="chart-title"
+  selector: 'gql-raised-in-vs-out-title',
+  imports: [GraphQLModule, ChartTitleComponent],
+  template: `
+    <chart-title
+      class="chart-title"
       [titleText]="title"
       [tooltipText]="tooltip"
     ></chart-title>
   `,
-    standalone: false
 })
 export class RaisedInVsOutTitleGQLComponent implements OnChanges {
   @Input() candidateId: string;
@@ -19,7 +22,7 @@ export class RaisedInVsOutTitleGQLComponent implements OnChanges {
 
   constructor(private candidateInfoGQL: CandidateInfoGQL) {}
 
-  ngOnChanges(changes: SimpleChanges): void  {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['candidateId']) {
       const candidateId = changes['candidateId'].currentValue;
       this.update(candidateId);
@@ -29,24 +32,31 @@ export class RaisedInVsOutTitleGQLComponent implements OnChanges {
   update(candidateId: string) {
     this.candidateId = candidateId;
 
-    if (!this.candidateId) { return; }
+    if (!this.candidateId) {
+      return;
+    }
 
-    this.candidateInfoGQL.watch({
-      candidateId: this.candidateId,
-    }, {
-      // errorPolicy: 'all',
-    }).valueChanges.subscribe( (result: any) => {
-      const response: CandidateInfo = result.data;
+    this.candidateInfoGQL
+      .watch(
+        {
+          candidateId: this.candidateId,
+        },
+        {
+          // errorPolicy: 'all',
+        }
+      )
+      .valueChanges.subscribe((result: any) => {
+        const response: CandidateInfo = result.data;
 
-      const district = response?.candidate?.district;
-      const jurisdictionComplete = district ? `District ${district}` : 'City';
+        const district = response?.candidate?.district;
+        const jurisdictionComplete = district ? `District ${district}` : 'City';
 
-      this.title = `In vs. Out of ${jurisdictionComplete}`;
+        this.title = `In vs. Out of ${jurisdictionComplete}`;
 
-      const agency = response?.candidate?.agency;
-      const area = district ? `${agency}, District ${district}` : `${agency}`;
+        const agency = response?.candidate?.agency;
+        const area = district ? `${agency}, District ${district}` : `${agency}`;
 
-      this.tooltip = `Total campaign funds raised inside versus funds raised outside ${area}.`;
-    });
+        this.tooltip = `Total campaign funds raised inside versus funds raised outside ${area}.`;
+      });
   }
 }
