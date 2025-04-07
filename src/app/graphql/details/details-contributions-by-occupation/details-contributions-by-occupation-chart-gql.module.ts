9fa6f49c7185d6a4ad16c5db7a4f22d90b8183ad
Robert <robertgz@users.noreply.github.com>
2025-04-07 14:48:49 -0700

    Converted DetailsContributionsByOccupationGQLComponent to standalone.
    Updated stories file.
    
    Updated  ContributionsByOccupationChartGQLModule. This module can be removed after upstream components are updated.

--- src/app/graphql/details/details-contributions-by-occupation/details-contributions-by-occupation-chart-gql.module.ts
+++ src/app/graphql/details/details-contributions-by-occupation/details-contributions-by-occupation-chart-gql.module.ts
@@ -1,22 +1,11 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { GraphQLModule } from '../../graphql.module';
-import { ChartTitleModule, DetailsContributionsByOccupationHeaderModule } from 'lib-ui-components';
-import { RaisedByIndustryBarModule } from 'lib-ui-charts';
-import { ContributionsGroupedByOccupationGQLQuery } from './details-contributions-by-occupation-table-gql/details-contributions-by-occupation-gql.query';
 import { DetailsContributionsByOccupationGQLComponent } from './details-contributions-by-occupation-table-gql/details-contributions-by-occupation-gql.component';
 
-@NgModule({ declarations: [
-        DetailsContributionsByOccupationGQLComponent,
-    ],
-    exports: [
-        DetailsContributionsByOccupationGQLComponent,
-    ],
-    bootstrap: [], imports: [CommonModule,
-        GraphQLModule,
-        ChartTitleModule,
-        RaisedByIndustryBarModule,
-        DetailsContributionsByOccupationHeaderModule], providers: [ContributionsGroupedByOccupationGQLQuery, provideHttpClient(withInterceptorsFromDi())] })
-export class ContributionsByOccupationChartGQLModule { }
+@NgModule({
+  declarations: [],
+  exports: [DetailsContributionsByOccupationGQLComponent],
+  bootstrap: [],
+  imports: [DetailsContributionsByOccupationGQLComponent],
+})
+export class ContributionsByOccupationChartGQLModule {}
--- src/app/graphql/details/details-contributions-by-occupation/details-contributions-by-occupation-table-gql/details-contributions-by-occupation-gql.component.stories.ts
+++ src/app/graphql/details/details-contributions-by-occupation/details-contributions-by-occupation-table-gql/details-contributions-by-occupation-gql.component.stories.ts
@@ -1,52 +1,47 @@
-// import { Meta, Story } from '@storybook/angular/types-6-0';
-import { moduleMetadata } from '@storybook/angular';
-import { CommonModule } from '@angular/common';
+import { Meta, applicationConfig, StoryObj } from '@storybook/angular';
+import { importProvidersFrom } from '@angular/core';
 
+import { GraphQLModule } from 'src/app/graphql/graphql.module';
 import { DetailsContributionsByOccupationGQLComponent } from './details-contributions-by-occupation-gql.component';
-import { ContributionsByOccupationChartGQLModule } from '../details-contributions-by-occupation-chart-gql.module';
 
-export default {
+const meta: Meta<DetailsContributionsByOccupationGQLComponent> = {
   title: 'Lib-gql/Details Contributions by Occupation',
   component: DetailsContributionsByOccupationGQLComponent,
   decorators: [
-    moduleMetadata({
-      declarations: [ ],
-      imports: [
-        CommonModule,
-        ContributionsByOccupationChartGQLModule,
-      ],
-      providers: [],
+    applicationConfig({
+      providers: [importProvidersFrom(GraphQLModule)],
     }),
-  ],  
-  argTypes: {
+  ],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<DetailsContributionsByOccupationGQLComponent>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Empty: Story = {
+  args: {
+    candidateId: '',
   },
 };
 
-
-export const Default = () => ({
-  props: {  },
-})
-
-export const Empty = () => ({
-  props: {
-    candidateId: ''
+export const BarbaraBry2020: Story = {
+  args: {
+    candidateId: 'be0a57fb-c0f0-bbd5-0d42-44a6560cbd21|2020',
   },
-})
+};
 
-export const BarbaraBry2020 = () => ({
-  props: {
-    candidateId: 'be0a57fb-c0f0-bbd5-0d42-44a6560cbd21|2020'
+export const JenniferCampbell2022: Story = {
+  args: {
+    candidateId: 'd3d1c6e7-0add-49f1-9b3d-e9289e7efcf6|2022',
   },
-})
+};
 
-export const JenniferCampbell2022 = () => ({
-  props: {
-    candidateId: 'd3d1c6e7-0add-49f1-9b3d-e9289e7efcf6|2022'
+export const MonicaMontgomerySteppe2022: Story = {
+  args: {
+    candidateId: '82cae978-49df-4a77-8793-0efffb7772b5|2022',
   },
-})
-
-export const MonicaMontgomerySteppe2022 = () => ({
-  props: {
-    candidateId: '82cae978-49df-4a77-8793-0efffb7772b5|2022'
-  },
-})
+};
