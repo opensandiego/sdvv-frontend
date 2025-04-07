4ce5f27d401df23a9f23c392a1ee7b49a52072bf
Robert <robertgz@users.noreply.github.com>
2025-04-07 13:59:11 -0700

    Converted DetailsContributionsByLocationComponent to standalone.
    Updated stories file.
    
    Updated  DetailsContributionsByLocationGQLModule. This module can be removed after upstream components are updated.

--- src/app/graphql/details/details-contributions-by-location/details-contributions-by-location-gql.module.ts
+++ src/app/graphql/details/details-contributions-by-location/details-contributions-by-location-gql.module.ts
@@ -1,17 +1,11 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { RaisedByLocationBarModule } from 'lib-ui-charts';
-import { GraphQLModule } from '../../graphql.module';
 import { DetailsContributionsByLocationComponent } from './details-contributions-by-location-gql.component';
-import { ContributionsGroupedByLocationGQL } from './details-contributions-by-location-gql.query';
 
-@NgModule({ declarations: [
-        DetailsContributionsByLocationComponent,
-    ],
-    exports: [DetailsContributionsByLocationComponent,],
-    bootstrap: [], imports: [CommonModule,
-        GraphQLModule,
-        RaisedByLocationBarModule], providers: [ContributionsGroupedByLocationGQL, provideHttpClient(withInterceptorsFromDi())] })
-export class DetailsContributionsByLocationGQLModule { }
+@NgModule({
+  declarations: [],
+  exports: [DetailsContributionsByLocationComponent],
+  bootstrap: [],
+  imports: [DetailsContributionsByLocationComponent],
+})
+export class DetailsContributionsByLocationGQLModule {}
