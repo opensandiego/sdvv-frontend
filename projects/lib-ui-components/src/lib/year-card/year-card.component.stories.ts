50a5f9d3c37d25782e103dd7081cd67e7537d6ef
Robert <robertgz@users.noreply.github.com>
2025-04-06 23:59:58 -0700

    Converted YearCardComponent to standalone.
    Updated stories file.
    
    Updated  YearCardModule. This module can be removed after upstream components are updated.

--- projects/lib-ui-components/src/lib/year-card/year-card.component.stories.ts
+++ projects/lib-ui-components/src/lib/year-card/year-card.component.stories.ts
@@ -1,67 +1,64 @@
-// import { Meta, Story } from '@storybook/angular/types-6-0';
-import { moduleMetadata } from '@storybook/angular';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
 
-import { YearCardModule } from './year-card.module';
 import { YearCardComponent } from './year-card.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-export default {
+const meta: Meta<YearCardComponent> = {
   title: 'Lib UI Components/Year Card',
   component: YearCardComponent,
   decorators: [
-    moduleMetadata({
-      declarations: [ 
-      ],
-      imports: [
-        YearCardModule,
-      ],
-      providers: [      ],
+    applicationConfig({
+      providers: [provideAnimations()],
     }),
-  ],  
+  ],
 };
 
-export const Default = () => ({
-  props: { },
-});
+export default meta;
+type Story = StoryObj<YearCardComponent>;
 
-export const Year2022 = () => ({
-  props: {
+export const Default: Story = {
+  args: {},
+};
+
+export const Year2022: Story = {
+  args: {
     year: '2022',
     mayorCandidateCount: 0,
     cityCouncilCandidateCount: 7,
     cityAttorneyCandidateCount: 0,
   },
-});
+};
 
-export const Year2020 = () => ({
-  props: {
+export const Year2020: Story = {
+  args: {
     year: '2020',
     mayorCandidateCount: 14,
     cityCouncilCandidateCount: 17,
     cityAttorneyCandidateCount: 19,
   },
-});
+};
 
-export const Year2018 = () => ({
-  props: {
+export const Year2018: Story = {
+  args: {
     year: '2018',
     mayorCandidateCount: 0,
     cityCouncilCandidateCount: 28,
     cityAttorneyCandidateCount: 0,
   },
-});
+};
 
-export const Year2018Partial = () => ({
-  props: {
+export const Year2018Partial: Story = {
+  args: {
     year: '2018',
     mayorCandidateCount: undefined,
     cityCouncilCandidateCount: 28,
     cityAttorneyCandidateCount: null,
   },
-});
+};
 
-export const Year2018Empty = () => ({
-  props: {
+export const Year2018Empty: Story = {
+  args: {
     mayorCandidateCount: undefined,
     cityAttorneyCandidateCount: null,
   },
-});
+};
--- projects/lib-ui-components/src/lib/year-card/year-card.module.ts
+++ projects/lib-ui-components/src/lib/year-card/year-card.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
 import { YearCardComponent } from './year-card.component';
 
 @NgModule({
-  declarations: [
-    YearCardComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-  ],
-  exports: [
-    YearCardComponent,
-  ]
+  declarations: [],
+  imports: [YearCardComponent],
+  exports: [YearCardComponent],
 })
-export class YearCardModule { }
+export class YearCardModule {}
