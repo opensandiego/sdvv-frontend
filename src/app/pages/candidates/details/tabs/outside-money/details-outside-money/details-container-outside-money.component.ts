60ff56dc13a97354f0679660b34cfb43328e5539
Robert <robertgz@users.noreply.github.com>
2025-04-07 19:52:14 -0700

    Converted DetailsTabOutsideMoneyComponent, DetailsContainerOutsideMoneyComponent to standalone.
    Updated stories file.
    
    Updated CandidateDetailsTabOutsideMoneyModule. This module can be removed after upstream components are updated.

--- src/app/pages/candidates/details/tabs/outside-money/details-outside-money/details-container-outside-money.component.ts
+++ src/app/pages/candidates/details/tabs/outside-money/details-outside-money/details-container-outside-money.component.ts
@@ -1,5 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { OutsideMoneyStackedBarComponent } from 'lib-ui-charts';
+import { TopCategoriesTableComponent } from 'src/app/components/top-categories-table/top-categories-table.component';
 import { globals } from 'src/app/globals';
 
 export interface IndependentCommittees {
@@ -11,15 +15,20 @@ export interface IndependentCommittees {
 }
 
 @Component({
-    selector: 'details-outside-money',
-    templateUrl: './details-container-outside-money.component.html',
-    styleUrls: ['./details-container-outside-money.component.scss'],
-    standalone: false
+  selector: 'details-outside-money',
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    OutsideMoneyStackedBarComponent,
+    TopCategoriesTableComponent,
+  ],
+  templateUrl: './details-container-outside-money.component.html',
+  styleUrls: ['./details-container-outside-money.component.scss'],
 })
 export class DetailsContainerOutsideMoneyComponent {
   @Input() oppositionCommittees: IndependentCommittees[];
   @Input() supportCommittees: IndependentCommittees[];
-  
+
   hoveredCommittee: string = null;
   oppositionColor = globals.expendituresInOppositionColor;
   supportColor = globals.expendituresInSupportColor;
@@ -33,11 +42,10 @@ export class DetailsContainerOutsideMoneyComponent {
     title: 'Expenditures in Support',
     categories: [],
   };
-  
+
   faCircle = faCircle;
 
-  committeeHoveredOver(committee){
+  committeeHoveredOver(committee) {
     this.hoveredCommittee = committee;
   }
-  
 }
--- src/app/pages/candidates/details/tabs/outside-money/outside-money-tab.module.ts
+++ src/app/pages/candidates/details/tabs/outside-money/outside-money-tab.module.ts
@@ -1,28 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GraphQLModule } from 'src/app/graphql/graphql.module';
 import { DetailsTabOutsideMoneyComponent } from './details-tab-outside-money/details-tab-outside-money.component';
 import { DetailsContainerOutsideMoneyComponent } from './details-outside-money/details-container-outside-money.component';
-import { OutsideMoneyStackedBarModule } from 'lib-ui-charts';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TopCategoriesTableComponent } from 'src/app/components/top-categories-table/top-categories-table.component';
-import { DetailsTabTitleModule } from 'lib-ui-components';
 
 @NgModule({
-  declarations: [
-    DetailsContainerOutsideMoneyComponent,
-    DetailsTabOutsideMoneyComponent,
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    FontAwesomeModule,
-    GraphQLModule,
-    DetailsTabTitleModule,
-    OutsideMoneyStackedBarModule,
-    TopCategoriesTableComponent,
-  ],
-  exports: [
     DetailsTabOutsideMoneyComponent,
+    DetailsContainerOutsideMoneyComponent,
   ],
+  exports: [DetailsTabOutsideMoneyComponent],
 })
-export class CandidateDetailsTabOutsideMoneyModule { }
+export class CandidateDetailsTabOutsideMoneyModule {}
