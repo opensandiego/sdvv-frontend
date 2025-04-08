63a765e68aa5dd0ad4957da9ab1ae27654de3b87
Robert <robertgz@users.noreply.github.com>
2025-04-07 20:24:19 -0700

    Converted components to standalone. CandidateDetailsContainerComponent
    Updated stories file.
    
    Updated CandidateDetailsContainerModule. This module can be removed after upstream components are updated.

--- src/app/pages/candidates/details/candidate-details/candidate-details-container.component.ts
+++ src/app/pages/candidates/details/candidate-details/candidate-details-container.component.ts
@@ -1,10 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { CandidateDetailsHeaderGQLComponent } from 'src/app/graphql/candidate-details-header-gql/candidate-details-header-gql.component';
+import { DetailsTabRaisedVSpentComponent } from '../tabs/raised-spent/details-tab-raised-v-spent/details-tab-raised-v-spent.component';
+import { ContributionsByOccupationTabComponent } from '../tabs/contributions-by-occupation/contributions-by-occupation-tab.component';
+import { ContributionsByLocationTabComponent } from '../tabs/contributions-by-location/contributions-by-location-tab.component';
 
 @Component({
-    selector: 'candidate-details-container',
-    templateUrl: './candidate-details-container.component.html',
-    styleUrls: ['./candidate-details-container.component.scss'],
-    standalone: false
+  selector: 'candidate-details-container',
+  imports: [
+    CommonModule,
+    MatTooltipModule,
+    MatTabsModule,
+    RouterModule,
+    CandidateDetailsHeaderGQLComponent,
+    DetailsTabRaisedVSpentComponent,
+    ContributionsByOccupationTabComponent,
+    ContributionsByLocationTabComponent,
+  ],
+  templateUrl: './candidate-details-container.component.html',
+  styles: [
+    `
+      .candidate-details {
+        padding: 20px 20px;
+        margin: 0em;
+
+        display: flex;
+        flex-direction: column;
+      }
+    `,
+  ],
 })
 export class CandidateDetailsContainerComponent {
   @Input() candidateId: string;
