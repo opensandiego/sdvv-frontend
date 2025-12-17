3614bc4e9d01e93e54acde21a645a71626f4a024
Robert <robertgz@users.noreply.github.com>
2025-12-16 22:18:21 -0800

    refactor: remove unused module files in lib-ui-components

--- projects/lib-ui-components/src/lib/details-tab-title/details-tab-title.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DetailsTabTitleComponent } from './details-tab-title.component';
-
-@NgModule({
-  declarations: [],
-  imports: [DetailsTabTitleComponent],
-  exports: [DetailsTabTitleComponent],
-})
-export class DetailsTabTitleModule {}
