220b234bc006e54123c9d08d8a7712a6cca794ea
Robert <robertgz@users.noreply.github.com>
2026-04-28 14:30:58 -0700

    feat: add AngularEChartWrapperComponent

---
+++ src/app/components/charts/angular/angular-echart-wrapper.component.ts
@@ -0,0 +1,68 @@
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  type AfterViewInit,
+  type OnDestroy,
+  inject,
+  PLATFORM_ID,
+  input,
+  output,
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import * as echarts from 'echarts';
+
+@Component({
+  standalone: true,
+  selector: 'angular-echarts',
+  template: `
+    <div
+      #chartContainer
+      [style.width]="width()"
+      [style.height]="height()"
+    ></div>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+})
+export class AngularEChartWrapperComponent implements AfterViewInit, OnDestroy {
+  private platformId = inject(PLATFORM_ID);
+
+  @ViewChild('chartContainer') chartContainer!: ElementRef;
+
+  options = input<echarts.EChartsOption>({});
+  width = input('100%');
+  height = input('400px');
+
+  chartClick = output<any>();
+
+  private chart: echarts.ECharts | null = null;
+  private resizeHandler = () => this.chart?.resize();
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.chart = echarts.init(this.chartContainer.nativeElement);
+      this.chart.setOption(this.options());
+
+      // listen for ECharts 'click' events and emit via the output
+      this.chart.on('click', (params) => {
+        this.chartClick.emit(params);
+      });
+
+      window.addEventListener('resize', this.resizeHandler);
+    }
+  }
+
+  ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.removeEventListener('resize', this.resizeHandler);
+      this.chart?.off('click'); 
+      this.chart?.dispose();
+    }
+  }
+}
