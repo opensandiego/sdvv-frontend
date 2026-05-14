662f3c3c2b6de807880e6256a15e11708bfdb257
Robert <robertgz@users.noreply.github.com>
2026-05-14 15:32:38 -0700

    refactor: move data source and last update into DataSourcesComponent, move to footer

--- src/app/routed/summary/summary.component.ts
+++ src/app/routed/summary/summary.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { globals } from 'src/app/globals';
-import { LastUpdatedComponent } from 'src/app/graphql/last-updated/last-updated.component';
 
 @Component({
-  imports: [RouterModule, LastUpdatedComponent],
+  imports: [RouterModule],
   selector: 'summary-item',
   template: `<div class="ftm">
     <h1>{{ title }}</h1>
     <h3>{{ subTitle }}</h3>
     <p class="ftm_caption">{{ caption }}</p>
     <router-outlet></router-outlet>
-    <div class="sources">
-      <p>
-        All of our data is sourced directly from the
-        <a href="https://efile.sandiego.gov/" target="_blank"
-          >City of San Diego Electronic Filing System.</a
-        >
-      </p>
-      <last-updated></last-updated>
-    </div>
   </div> `,
   styleUrls: ['./summary.component.scss'],
 })
