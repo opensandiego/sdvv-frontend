b5d2e5a2c1200b3fd64b569377313a08a455ef50
Robert <robertgz@users.noreply.github.com>
2026-05-13 19:03:51 -0700

    fix(footer): fix About and FAQ links in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
